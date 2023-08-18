a92aac39eff5e9a87c52ceaa5acbade3846259a4
Johan Blomgren <blommish@users.noreply.github.com>
2023-08-18 14:15:34 +0200

    Skall for frontend-server (#1)

---
+++ src/backend/logger.ts
@@ -0,0 +1,43 @@
+import winston from 'winston';
+import { Request } from 'express';
+
+const logger = winston.createLogger({
+    transports: [
+        new winston.transports.Console({
+            format: winston.format.json(),
+        }),
+    ],
+});
+
+const prefix = (req: Request) => {
+    return `${req.method} - ${req.originalUrl}`;
+};
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const utledMetadata = (req: Request, error?: any) => {
+    const callId = req.header('nav-call-id');
+    const requestId = req.header('x-request-id');
+
+    return {
+        ...(callId ? { x_callId: callId } : {}),
+        ...(requestId ? { x_requestId: requestId } : {}),
+        ...(error ? { error: error } : {}),
+    };
+};
+
+export const logInfo = (message: string, req: Request) => {
+    const melding = `${prefix(req)}: ${message}`;
+    const meta = utledMetadata(req);
+
+    logger.info(melding, meta);
+};
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const logWarn = (message: string, req: Request, error?: any) => {
+    const melding = `${prefix(req)}: ${message}`;
+    const meta = utledMetadata(req, error);
+
+    logger.warn(melding, meta);
+};
+
+export default logger;
