b3b48fff6bc04843163254d9af7651c48d2a7925
sarahjelle <sara.hjelle@bekk.no>
2023-08-23 16:04:42 +0200

    Skal legge til router

--- src/frontend/index.tsx
+++ src/frontend/index.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 const rootElement = document.getElementById('app');
 const root = createRoot(rootElement!);
-root.render(<App />);
+
+root.render(
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+        <Routes>
+            <Route path={'/*'} element={<App />} />
+        </Routes>
+    </BrowserRouter>
+);
