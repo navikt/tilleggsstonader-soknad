f754651979bb93fbdc1684ac50ea9df3afbd12ce
sarahjelle <sara.hjelle@bekk.no>
2023-08-24 17:12:33 +0200

    LocaleTekst skal ligge i componentsmappe

--- src/frontend/barnetilsyn/typer/LocaleTekst.tsx
+++
@@ -1,7 +0,0 @@
-import { useSpråk } from '../../context/SpråkContext';
-import { TekstElement } from '../../typer/tekst';
-
-export const Tekst: React.FC<{ tekst: TekstElement }> = ({ tekst }) => {
-    const { locale } = useSpråk();
-    return <>{tekst[locale]}</>;
-};
