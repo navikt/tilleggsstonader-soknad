b2ae53d94931656d3a40c243081866391462b83a
sarahjelle <sara.hjelle@bekk.no>
2023-08-25 13:00:13 +0200

    Skal ta i bruk Side for å strukturere innhold

--- src/frontend/barnetilsyn/steg/1-personalia/Personalia.tsx
+++ src/frontend/barnetilsyn/steg/1-personalia/Personalia.tsx
@@ -1,5 +1,20 @@
-const Personalia = () => {
-    return <p>Personalia</p>;
-};
+import { GuidePanel, Heading } from '@navikt/ds-react';
 
+import { LocaleTekst } from '../../../components/LocaleTekst';
+import Side from '../../../components/Side';
+import { Stønadstype } from '../../../typer/stønadstyper';
+import { personaliaTekster } from '../../tekster/personalia';
+
+const Personalia = () => {
+    return (
+        <Side stegtittel={personaliaTekster.steg_tittel} stønadstype={Stønadstype.barnetilsyn}>
+            <Heading size="medium">
+                <LocaleTekst tekst={personaliaTekster.innhold_tittel} />
+            </Heading>
+            <GuidePanel>
+                <LocaleTekst tekst={personaliaTekster.guide_innhold} />
+            </GuidePanel>
+        </Side>
+    );
+};
 export default Personalia;
