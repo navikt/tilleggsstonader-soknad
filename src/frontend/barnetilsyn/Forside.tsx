67f336c57f5b19d17094b2db1cfbcdf7cde389ce
sarahjelle <sara.hjelle@bekk.no>
2023-08-28 09:44:57 +0200

    Skal sette opp forside barnetilsyn med innhold

--- src/frontend/barnetilsyn/Forside.tsx
+++ src/frontend/barnetilsyn/Forside.tsx
@@ -1,5 +1,125 @@
+import { useNavigate } from 'react-router';
+import { styled } from 'styled-components';
+
+import {
+    Accordion,
+    Alert,
+    BodyShort,
+    Button,
+    Checkbox,
+    Heading,
+    Label,
+    List,
+} from '@navikt/ds-react';
+import { ABreakpointMd } from '@navikt/ds-tokens/dist/tokens';
+
+import { forsideTekster } from './tekster/forside';
+import { LocaleTekst } from '../components/LocaleTekst';
+import { PellePanel } from '../components/PellePanel/PellePanel';
+
+const Container = styled.div`
+    padding: 2rem 1rem;
+    display: flex;
+    flex-direction: column;
+    gap: 2.5rem;
+
+    @media (min-width: ${ABreakpointMd}) {
+        max-width: 35rem;
+        margin: auto;
+        padding: 2rem 0;
+    }
+`;
+
+const TextContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+`;
+
+const KnappeContainer = styled(BodyShort)`
+    display: flex;
+    gap: 1rem;
+    justify-content: center;
+`;
+
 const Forside = () => {
-    return <p>Forside</p>;
+    const navigate = useNavigate();
+    return (
+        <Container>
+            <PellePanel poster>
+                <TextContainer>
+                    <Label>
+                        <LocaleTekst tekst={forsideTekster.veileder_tittel} />
+                    </Label>
+                    <BodyShort>
+                        <LocaleTekst tekst={forsideTekster.veileder_innhold1} />
+                    </BodyShort>
+                    <BodyShort>
+                        <LocaleTekst tekst={forsideTekster.veileder_innhold2} />
+                    </BodyShort>
+                </TextContainer>
+            </PellePanel>
+            <Alert variant="info">
+                <Heading size="small">
+                    <LocaleTekst tekst={forsideTekster.mottatt_faktura_alert_tittel} />
+                </Heading>
+                <BodyShort size="medium">
+                    <LocaleTekst tekst={forsideTekster.mottatt_faktura_alert_innhold} />
+                </BodyShort>
+            </Alert>
+            <List title="Dine plikter">
+                <LocaleTekst tekst={forsideTekster.dine_plikter_innhold} />
+            </List>
+            <Accordion>
+                <Accordion.Item>
+                    <Accordion.Header>
+                        <LocaleTekst tekst={forsideTekster.utgifter_som_dekkes_tittel} />
+                    </Accordion.Header>
+                    <Accordion.Content>
+                        <TextContainer>
+                            <BodyShort>
+                                <LocaleTekst tekst={forsideTekster.utgifter_som_dekkes_innhold1} />
+                            </BodyShort>
+                            <BodyShort>
+                                <LocaleTekst tekst={forsideTekster.utgifter_som_dekkes_innhold2} />
+                            </BodyShort>
+                        </TextContainer>
+                    </Accordion.Content>
+                </Accordion.Item>
+                <Accordion.Item>
+                    <Accordion.Header>
+                        <LocaleTekst tekst={forsideTekster.info_som_hentes_tittel} />
+                    </Accordion.Header>
+                    <Accordion.Content>
+                        <LocaleTekst tekst={forsideTekster.info_som_hentes_innhold1} />
+                        <List>
+                            <LocaleTekst tekst={forsideTekster.info_som_hentes_innhold2} />
+                        </List>
+                        <LocaleTekst tekst={forsideTekster.info_som_hentes_innhold3} />
+                    </Accordion.Content>
+                </Accordion.Item>
+                <Accordion.Item>
+                    <Accordion.Header>
+                        <LocaleTekst tekst={forsideTekster.dokumentasjon_utgifter_tittel} />
+                    </Accordion.Header>
+                    <Accordion.Content>
+                        <LocaleTekst tekst={forsideTekster.dokumentasjon_utgifter_innhold} />
+                    </Accordion.Content>
+                </Accordion.Item>
+            </Accordion>
+            <div>
+                <Label>
+                    <LocaleTekst tekst={forsideTekster.vi_stoler_tittel} />
+                </Label>
+                <Checkbox>
+                    <LocaleTekst tekst={forsideTekster.vi_stoler_innhold} />
+                </Checkbox>
+            </div>
+            <KnappeContainer>
+                <Button onClick={() => navigate('/barnetilsyn/personalia')}>Start søknad</Button>
+            </KnappeContainer>
+        </Container>
+    );
 };
 
 export default Forside;
--- src/frontend/barnetilsyn/tekster/forside.ts
+++ src/frontend/barnetilsyn/tekster/forside.ts
@@ -4,4 +4,62 @@ export const forsideTekster: ForsideInnhold = {
     veileder_tittel: {
         nb: 'Hei [0]',
     },
+    veileder_innhold1: {
+        nb: 'Jeg er her for å veilede deg gjennom søknaden.',
+    },
+    veileder_innhold2: {
+        nb: 'Svarene dine lagres underveis, slik at du trygt kan gå tilbake og endre dem. En påbegynt søknad lagres i én måned på Ditt NAV.',
+    },
+    mottatt_faktura_alert_tittel: {
+        nb: 'Søker du om støtte til pass av barn for nytt skole/barnehageår?',
+    },
+    mottatt_faktura_alert_innhold: {
+        nb: 'Du må dokumentere utgiftene til barnepass for perioden du søker for. Vi anbefaler at du venter med å søke til du har fakturaen. ',
+    },
+    dine_plikter_tittel: {
+        nb: 'Dine plikter',
+    },
+    dine_plikter_innhold: {
+        nb: [
+            'Du må gi oss riktige opplysninger i søknaden.',
+            'Du må dokumentere dine utgifter til pass av barn med faktura som inneholder beløp og periode.',
+            'Du må gi beskjed til oss hvis situasjonen din endrer seg. ',
+            'Hvis du får penger du ikke har rett på, må du du være forberedt på å måtte betale dem tilbake.',
+            'Du kan bare søke om støtte til pass av barn, hvis ingen andre har fått dekket utgiftene til pass for samme barn. ',
+        ],
+    },
+    utgifter_som_dekkes_tittel: {
+        nb: 'Hvilke utgifter dekker vi?',
+    },
+    utgifter_som_dekkes_innhold1: {
+        nb: 'Utgifter til mat og bleier dekkes ikke.',
+    },
+    utgifter_som_dekkes_innhold2: {
+        nb: 'Vi dekker 64 prosent av utgiftene du har til pass av barn, opp til en maksimumssats.',
+    },
+    info_som_hentes_tittel: {
+        nb: 'Informasjon vi henter om deg',
+    },
+    info_som_hentes_innhold1: {
+        nb: 'I tillegg til den informasjonen du oppgir i søknaden, henter vi: ',
+    },
+    info_som_hentes_innhold2: {
+        nb: [
+            'adressen din og opplysninger om dine barn fra Folkeregisteret',
+            'informasjon om aktivitet eller utdanning som er godkjent av NAV',
+        ],
+    },
+    info_som_hentes_innhold3: {
+        nb: 'Slik behandler vi personopplysningene dine',
+    },
+    dokumentasjon_utgifter_tittel: {
+        nb: 'Dokumentasjon av utgifter',
+    },
+    dokumentasjon_utgifter_innhold: {
+        nb: 'Du må dokumentere utgiftene til barnepass med en faktura som inneholder barnets navn, beløp og periode. Vi dekker ikke utgifter til mat og bleier.',
+    },
+    vi_stoler_tittel: { nb: 'Vi stoler på deg' },
+    vi_stoler_innhold: {
+        nb: 'Jeg bekrefter at jeg kjenner til mine plikter og vil svare så godt jeg kan på spørsmålene i søknaden.',
+    },
 };
