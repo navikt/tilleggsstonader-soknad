624ec6a3f95f06ccb922b593cd0eb713b1a1d143
sarahjelle <sara.hjelle@bekk.no>
2023-08-28 12:14:39 +0200

    Refaktor. Skal ha stor I for lesbarhet

--- src/frontend/components/Side.tsx
+++ src/frontend/components/Side.tsx
@@ -31,7 +31,7 @@ const Container = styled.div`
     }
 `;
 
-const Stegindikator = styled.div`
+const StegIndikator = styled.div`
     display: flex;
     flex-direction: column;
     gap: 0.5rem;
@@ -69,14 +69,14 @@ const Side: React.FC<Props> = ({ stønadstype, stegtittel, children }) => {
 
     return (
         <Container>
-            <Stegindikator>
+            <StegIndikator>
                 <Heading size="medium" as="h2">
                     <LocaleTekst tekst={stegtittel} />
                 </Heading>
                 <BodyShort size="small">
                     Steg {aktivtSteg} av {routes.length - 1}
                 </BodyShort>
-            </Stegindikator>
+            </StegIndikator>
             <Innhold>{children}</Innhold>
             <KnappeContainer>
                 <Button variant="secondary" onClick={navigerTilForrigeSide}>
