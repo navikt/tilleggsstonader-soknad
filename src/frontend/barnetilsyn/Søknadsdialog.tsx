083246532604ab6c3f11ab1bba1eb64134a03ea6
sarahjelle <sara.hjelle@bekk.no>
2023-08-28 17:04:35 +0200

    Skal legge søknadsprovider samme sted som språkcontext

--- src/frontend/barnetilsyn/Søknadsdialog.tsx
+++ src/frontend/barnetilsyn/Søknadsdialog.tsx
@@ -4,17 +4,16 @@ import Forside from './Forside';
 import Personalia from './steg/1-personalia/Personalia';
 import { fellesTeksterBT } from './tekster/felles';
 import { Banner } from '../components/Banner';
-import { SøknadProvider } from '../context/SøknadContext';
 
 const Søknadsdialog: React.FC = () => {
     return (
-        <SøknadProvider>
+        <>
             <Banner tittel={fellesTeksterBT.banner} />
             <Routes>
                 <Route path={'*'} element={<Forside />} />
                 <Route path={'/personalia'} element={<Personalia />} />
             </Routes>
-        </SøknadProvider>
+        </>
     );
 };
 
