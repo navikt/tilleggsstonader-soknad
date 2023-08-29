384a0cadd480c3ab7a9d19f9e4096a0ad14b8179
sarahjelle <sara.hjelle@bekk.no>
2023-08-29 14:59:46 +0200

    Skal kunne huke av for barn som det skal søkes for

--- src/frontend/barnetilsyn/tekster/dineBarn.ts
+++ src/frontend/barnetilsyn/tekster/dineBarn.ts
@@ -2,10 +2,16 @@ import { TekstElement } from '../../typer/tekst';
 
 interface DineBarnInnhold {
     steg_tittel: TekstElement<string>;
+    guide_innhold: TekstElement<string>;
+    hvilke_barn_spm: TekstElement<string>;
 }
 
 export const dineBarnTekster: DineBarnInnhold = {
     steg_tittel: {
         nb: 'Dine barn',
     },
+    guide_innhold: {
+        nb: 'Vi henter opplysninger om barn fra folkeregisteret. Du kan ikke legge til barn her.  Hvis du vil melde fra om feil, må du gjøre det på skatteetaten.no.',
+    },
+    hvilke_barn_spm: { nb: 'Hvilke barn søker du om støtte til pass for?' },
 };
