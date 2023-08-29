5ad714457298375ae17672efdbdf47a7fc16d984
sarahjelle <sara.hjelle@bekk.no>
2023-08-29 12:38:49 +0200

    Skal redirecte til start dersom bruker ikke har bekreftet

--- src/frontend/barnetilsyn/Søknadsdialog.tsx
+++ src/frontend/barnetilsyn/Søknadsdialog.tsx
@@ -6,7 +6,9 @@ import Personalia from './steg/1-personalia/Personalia';
 import Aktivitet from './steg/3-aktivitet/Aktivitet';
 import DineBarn from './steg/4-dine-barn/DineBarn';
 import { Banner } from '../components/Banner';
+import RedirectTilStart from '../components/RedirectTilStart';
 import { fellesTekster } from '../tekster/felles';
+import { Stønadstype } from '../typer/stønadstyper';
 
 const Søknadsdialog: React.FC = () => {
     return (
@@ -14,11 +16,46 @@ const Søknadsdialog: React.FC = () => {
             <Banner tittel={fellesTekster.banner_bt} />
             <Routes>
                 <Route path={'*'} element={<Forside />} />
-                <Route path={'/personalia'} element={<Personalia />} />
-                <Route path={'/hovedytelse'} element={<Personalia />} />
-                <Route path={'/aktivitet'} element={<Aktivitet />} />
-                <Route path={'/dine-barn'} element={<DineBarn />} />
-                <Route path={'/kvittering'} element={<Kvittering />} />
+                <Route
+                    path={'/personalia'}
+                    element={
+                        <RedirectTilStart stønadstype={Stønadstype.barnetilsyn}>
+                            <Personalia />
+                        </RedirectTilStart>
+                    }
+                />
+                <Route
+                    path={'/hovedytelse'}
+                    element={
+                        <RedirectTilStart stønadstype={Stønadstype.barnetilsyn}>
+                            <Personalia />
+                        </RedirectTilStart>
+                    }
+                />
+                <Route
+                    path={'/aktivitet'}
+                    element={
+                        <RedirectTilStart stønadstype={Stønadstype.barnetilsyn}>
+                            <Aktivitet />
+                        </RedirectTilStart>
+                    }
+                />
+                <Route
+                    path={'/dine-barn'}
+                    element={
+                        <RedirectTilStart stønadstype={Stønadstype.barnetilsyn}>
+                            <DineBarn />
+                        </RedirectTilStart>
+                    }
+                />
+                <Route
+                    path={'/kvittering'}
+                    element={
+                        <RedirectTilStart stønadstype={Stønadstype.barnetilsyn}>
+                            <Kvittering />
+                        </RedirectTilStart>
+                    }
+                />
             </Routes>
         </>
     );
