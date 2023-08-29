7974b9b4bd4dc487576eae49b9de7b92a738d388
Johan Blomgren <blommish@users.noreply.github.com>
2023-08-29 16:23:56 +0200

    Lagt til tokenx for å kunne kalle på backend

---
+++ src/backend/miljø.ts
@@ -0,0 +1,33 @@
+import logger from './logger';
+
+const lokaltMiljø = {
+    apiUrl: 'http://localhost:8001',
+    oauthCallbackUri: 'https://localhost:8080/tilleggsstonader/soknad/soknad/oauth2/callback',
+};
+
+const devMiljø = {
+    apiUrl: 'http://tilleggsstonader-soknad-api/tilleggsstonader/soknad/soknad-api',
+    oauthCallbackUri:
+        'https://tilleggsstonader.ekstern.dev.nav.no/tilleggsstonader/soknad/oauth2/callback',
+};
+
+const prodMiljø = {
+    apiUrl: 'http://tilleggsstonader-soknad-api/api',
+    oauthCallbackUri: 'https://www.nav.no/tilleggsstonader/soknad/soknad/oauth2/callback',
+};
+
+const initierMiljøvariabler = () => {
+    switch (process.env.ENV) {
+        case 'localhost':
+            return lokaltMiljø;
+        case 'dev':
+            return devMiljø;
+        case 'prod':
+            return prodMiljø;
+        default:
+            logger.warn('Mangler miljøvariabler - setter lokale variabler');
+            return lokaltMiljø;
+    }
+};
+
+export const miljø = initierMiljøvariabler();
--- webpack/webpack.development.js
+++ webpack/webpack.development.js
@@ -5,6 +5,12 @@ import { merge } from 'webpack-merge';
 import common from './webpack.common.js';
 
 const publicPath = process.env.PUBLIC_URL || '/';
+const brukMockLokalt = process.env.BRUK_MOCK_LOKALT && {
+    '/api': 'http://localhost:8001'
+};
+
+console.log(`publicPath=${publicPath}`)
+console.log(`brukMockLokalt=${process.env.BRUK_MOCK_LOKALT}`)
 
 const developmentConfig = merge(common, {
     mode: 'development',
@@ -17,6 +23,7 @@ const developmentConfig = merge(common, {
         historyApiFallback: {
             index: publicPath,
         },
+        proxy: brukMockLokalt
     },
     output: {
         filename: '[name].bundle.js',
