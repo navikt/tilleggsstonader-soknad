201be6bfeb5cdf67f28cc34335bec97af0ff793a
sarahjelle <sara.hjelle@bekk.no>
2023-08-29 11:24:11 +0200

    Skal legge til alert om at søknad er mottatt

--- src/frontend/barnetilsyn/Kvittering.tsx
+++ src/frontend/barnetilsyn/Kvittering.tsx
@@ -1,8 +1,9 @@
-import { Heading } from '@navikt/ds-react';
+import { Alert, Heading } from '@navikt/ds-react';
 
 import { kvitteringTekster } from './tekster/kvittering';
 import { Container } from '../components/Side';
 import LocaleTekst from '../components/Teksthåndtering/LocaleTekst';
+import LocaleTekstAvsnitt from '../components/Teksthåndtering/LocaleTekstAvsnitt';
 
 const Kvittering = () => {
     return (
@@ -10,6 +11,12 @@ const Kvittering = () => {
             <Heading size="medium" as="h2">
                 <LocaleTekst tekst={kvitteringTekster.steg_tittel} />
             </Heading>
+            <Alert variant="success">
+                <Heading size="small" as="h3">
+                    <LocaleTekst tekst={kvitteringTekster.søknad_mottatt_alert_tittel} />
+                </Heading>
+                <LocaleTekstAvsnitt tekst={kvitteringTekster.søknad_mottatt_alert_innhold} />
+            </Alert>
         </Container>
     );
 };
--- src/frontend/barnetilsyn/tekster/kvittering.ts
+++ src/frontend/barnetilsyn/tekster/kvittering.ts
@@ -2,10 +2,19 @@ import { TekstElement } from '../../typer/tekst';
 
 interface KvitteringInnhold {
     steg_tittel: TekstElement<string>;
+    søknad_mottatt_alert_tittel: TekstElement<string>;
+    søknad_mottatt_alert_innhold: TekstElement<string[]>;
 }
 
 export const kvitteringTekster: KvitteringInnhold = {
     steg_tittel: {
         nb: 'Kvittering',
+    },
+    søknad_mottatt_alert_tittel: { nb: 'Søknad mottatt' },
+    søknad_mottatt_alert_innhold: {
+        nb: [
+            'Vi har mottatt søknaden din om støtte til pass av barn. Saken din er nå til behandling hos NAV.',
+            'Vi vil ta kontakt med deg på telefon eller via Mitt NAV hvis vi trenger mer informasjon eller dokumentasjon fra deg.',
+        ],
     },
 };
