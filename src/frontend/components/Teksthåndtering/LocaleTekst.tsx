c4a1e807f10ab7e0868218dc04253ed30d64d221
sarahjelle <sara.hjelle@bekk.no>
2023-08-31 09:05:37 +0200

    Skal kunne sette inn variabel i LocaleTekst

--- src/frontend/components/Teksthåndtering/LocaleTekst.tsx
+++ src/frontend/components/Teksthåndtering/LocaleTekst.tsx
@@ -1,10 +1,15 @@
 import { useSpråk } from '../../context/SpråkContext';
 import { TekstElement } from '../../typer/tekst';
+import { hentBeskjedMedEttParameter } from '../../utils/tekster';
 
-const LocaleTekst: React.FC<{ tekst: TekstElement<string> }> = ({ tekst }) => {
+const LocaleTekst: React.FC<{ tekst: TekstElement<string>; argument0?: string }> = ({
+    tekst,
+    argument0,
+}) => {
     const { locale } = useSpråk();
+    const tekstStreng = tekst[locale];
 
-    return <>{tekst[locale]}</>;
+    return <>{argument0 ? hentBeskjedMedEttParameter(argument0, tekstStreng) : tekstStreng}</>;
 };
 
 export default LocaleTekst;
