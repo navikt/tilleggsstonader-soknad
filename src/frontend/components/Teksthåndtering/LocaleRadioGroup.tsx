8f1a393656d1292606bca749ec21d0723edb8ac4
sarahjelle <sara.hjelle@bekk.no>
2023-08-31 10:12:57 +0200

    Skal kunne send inn arg0 til LocaleRadio

--- src/frontend/components/Teksthåndtering/LocaleRadioGroup.tsx
+++ src/frontend/components/Teksthåndtering/LocaleRadioGroup.tsx
@@ -4,19 +4,30 @@ import { Radio, RadioGroup, RadioGroupProps as AkselRadioGroupProps } from '@nav
 
 import { useSpråk } from '../../context/SpråkContext';
 import { Radiogruppe } from '../../typer/tekst';
+import { hentBeskjedMedEttParameter } from '../../utils/tekster';
 
 interface RadioGroupProps<T>
     extends Omit<AkselRadioGroupProps, 'legend' | 'description' | 'children'> {
     tekst: Radiogruppe<T>;
     children?: React.ReactNode;
+    argument0?: string;
 }
-function LocaleRadioGroup<T>({ children, tekst, ...props }: RadioGroupProps<T>) {
+function LocaleRadioGroup<T>({ children, tekst, argument0, ...props }: RadioGroupProps<T>) {
     const { locale } = useSpråk();
 
     return (
         <RadioGroup
-            legend={tekst.header[locale]}
-            description={tekst.beskrivelse && tekst.beskrivelse[locale]}
+            legend={
+                argument0
+                    ? hentBeskjedMedEttParameter(argument0, tekst.header[locale])
+                    : tekst.header[locale]
+            }
+            description={
+                tekst.beskrivelse &&
+                (argument0
+                    ? hentBeskjedMedEttParameter(argument0, tekst.header[locale])
+                    : tekst.beskrivelse[locale])
+            }
             {...props}
         >
             {children}
