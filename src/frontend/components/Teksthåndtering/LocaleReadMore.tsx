c860b48d00d39723c8200d3bfd9fbf19b7eef5aa
eirarset <eirik.arseth@nav.no>
2023-08-31 09:24:45 +0200

    Refaktor. Bruker bodylong for spacing

--- src/frontend/components/Teksthåndtering/LocaleReadMore.tsx
+++ src/frontend/components/Teksthåndtering/LocaleReadMore.tsx
@@ -1,34 +1,24 @@
-import styled from 'styled-components';
-
-import { ReadMore } from '@navikt/ds-react';
+import { BodyLong, ReadMore } from '@navikt/ds-react';
 
 import { useSpråk } from '../../context/SpråkContext';
 import { LesMer, TekstElement } from '../../typer/tekst';
-
-const Flexbox = styled.div`
-    display: flex;
-    flex-direction: column;
-    gap: 1rem;
-`;
 
 const LocaleReadMore: React.FC<{ tekst: TekstElement<LesMer> }> = ({ tekst }) => {
     const { locale } = useSpråk();
 
     const innhold = tekst[locale].innhold;
 
-    if (Array.isArray(innhold)) {
-        return (
-            <ReadMore header={tekst[locale].header}>
-                <Flexbox>
-                    {innhold.map((avsnitt, indeks) => (
-                        <span key={indeks}>{avsnitt}</span>
-                    ))}
-                </Flexbox>
-            </ReadMore>
-        );
-    }
-
-    return <ReadMore header={tekst[locale].header}>{tekst[locale].innhold}</ReadMore>;
+    return (
+        <ReadMore header={tekst[locale].header}>
+            {Array.isArray(innhold)
+                ? innhold.map((avsnitt, indeks) => (
+                      <BodyLong key={indeks} spacing>
+                          {avsnitt}
+                      </BodyLong>
+                  ))
+                : innhold}
+        </ReadMore>
+    );
 };
 
 export default LocaleReadMore;
