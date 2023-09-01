d19a71938bf0a492a37bb343d3325956e8c7e647
eirarset <eirik.arseth@nav.no>
2023-09-01 10:50:38 +0200

    wip

--- src/frontend/barnetilsyn/steg/5-vedlegg/Vedlegg.tsx
+++ src/frontend/barnetilsyn/steg/5-vedlegg/Vedlegg.tsx
@@ -1,14 +1,23 @@
 import React from 'react';
 
-import { Heading } from '@navikt/ds-react';
+import { styled } from 'styled-components';
+
+import { UploadIcon } from '@navikt/aksel-icons';
+import { Accordion, BodyLong, BodyShort, Button, Heading, Label } from '@navikt/ds-react';
 
 import { PellePanel } from '../../../components/PellePanel/PellePanel';
 import Side from '../../../components/Side';
+import LocalePunktliste from '../../../components/Teksthåndtering/LocalePunktliste';
+import LocaleReadMore from '../../../components/Teksthåndtering/LocaleReadMore';
 import LocaleTekst from '../../../components/Teksthåndtering/LocaleTekst';
 import LocaleTekstAvsnitt from '../../../components/Teksthåndtering/LocaleTekstAvsnitt';
 import { Stønadstype } from '../../../typer/stønadstyper';
 import { vedleggTekster } from '../../tekster/vedlegg';
 
+const StyledKnapp = styled(Button)`
+    width: max-content;
+    margin-top: 1rem;
+`;
 const Vedlegg = () => {
     return (
         <Side stønadstype={Stønadstype.barnetilsyn} stegtittel={vedleggTekster.steg_tittel}>
@@ -18,6 +27,81 @@ const Vedlegg = () => {
             <PellePanel>
                 <LocaleTekstAvsnitt tekst={vedleggTekster.guide_innhold} />
             </PellePanel>
+            <section>
+                <Heading size={'small'}>
+                    <LocaleTekst tekst={vedleggTekster.pass_ronja_espen} />
+                </Heading>
+                <BodyShort>
+                    <LocaleTekst tekst={vedleggTekster.du_må_legge_ved} />
+                </BodyShort>
+                <LocalePunktliste innhold={vedleggTekster.vedlegg_espen_ronja} />
+                <BodyShort>
+                    <LocaleTekst tekst={vedleggTekster.samlet_faktura} />
+                </BodyShort>
+                <LocaleReadMore tekst={vedleggTekster.faktura_lesmer} />
+                <StyledKnapp icon={<UploadIcon title="a11y-title" />}>
+                    <LocaleTekst tekst={vedleggTekster.last_opp_faktura} />
+                </StyledKnapp>
+            </section>
+            <section>
+                <Heading size={'small'}>
+                    <LocaleTekst tekst={vedleggTekster.legeerklæring_espen_tittel} />
+                </Heading>
+                <BodyLong>
+                    <LocaleTekst tekst={vedleggTekster.legeerklæring_espen} />
+                </BodyLong>
+                <StyledKnapp icon={<UploadIcon title="a11y-title" />}>
+                    <LocaleTekst tekst={vedleggTekster.last_opp_legeerklæring} />
+                </StyledKnapp>
+            </section>
+            <Accordion>
+                <Accordion.Item>
+                    <Accordion.Header>
+                        <LocaleTekst
+                            tekst={
+                                vedleggTekster.accordians.har_ikke_vedlegg_digital
+                                    .har_ikke_vedlegg_digital
+                            }
+                        />
+                    </Accordion.Header>
+                    <Accordion.Content>
+                        <Label>
+                            <LocaleTekst
+                                tekst={
+                                    vedleggTekster.accordians.har_ikke_vedlegg_digital.slik_ta_bilde
+                                }
+                            />
+                        </Label>
+                        <LocalePunktliste
+                            innhold={
+                                vedleggTekster.accordians.har_ikke_vedlegg_digital.instruksjoner
+                            }
+                        />
+                        <Label>
+                            <LocaleTekst
+                                tekst={
+                                    vedleggTekster.accordians.har_ikke_vedlegg_digital
+                                        .ettet_tatt_bilde
+                                }
+                            />
+                        </Label>
+                        <LocalePunktliste
+                            innhold={
+                                vedleggTekster.accordians.har_ikke_vedlegg_digital
+                                    .instruksjoner_etter
+                            }
+                        />
+                    </Accordion.Content>
+                </Accordion.Item>
+                <Accordion.Item>
+                    <Accordion.Header>
+                        <LocaleTekst tekst={vedleggTekster.accordians.format_kvalitet.tittel} />
+                    </Accordion.Header>
+                    <Accordion.Content>
+                        <LocaleTekst tekst={vedleggTekster.accordians.format_kvalitet.innhold} />
+                    </Accordion.Content>
+                </Accordion.Item>
+            </Accordion>
         </Side>
     );
 };
--- src/frontend/barnetilsyn/tekster/vedlegg.ts
+++ src/frontend/barnetilsyn/tekster/vedlegg.ts
@@ -1,10 +1,70 @@
-import { TekstElement } from '../../typer/tekst';
+import { LesMer, TekstElement } from '../../typer/tekst';
 
 interface VedleggInnhold {
     steg_tittel: TekstElement<string>;
     innhold_tittel: TekstElement<string>;
     guide_innhold: TekstElement<string[]>;
+    du_må_legge_ved: TekstElement<string>;
+    samlet_faktura: TekstElement<string>;
+    faktura_lesmer: TekstElement<LesMer>;
+    last_opp_faktura: TekstElement<string>;
+    last_opp_legeerklæring: TekstElement<string>;
+
+    accordians: {
+        har_ikke_vedlegg_digital: {
+            har_ikke_vedlegg_digital: TekstElement<string>;
+            slik_ta_bilde: TekstElement<string>;
+            instruksjoner: TekstElement<string[]>;
+            ettet_tatt_bilde: TekstElement<string>;
+            instruksjoner_etter: TekstElement<string[]>;
+        };
+        format_kvalitet: {
+            tittel: TekstElement<string>;
+            innhold: TekstElement<string>;
+        };
+    };
+
+    // Hardkodet:
+    pass_ronja_espen: TekstElement<string>;
+    vedlegg_espen_ronja: TekstElement<string[]>;
+    legeerklæring_espen_tittel: TekstElement<string>;
+    legeerklæring_espen: TekstElement<string>;
 }
+
+const formatKvalitetAccordian: VedleggInnhold['accordians']['format_kvalitet'] = {
+    tittel: {
+        nb: 'Format og kvalitet på vedlegg',
+    },
+    innhold: {
+        nb: 'Du kan laste opp vedlegg png, pdf eller jpeg. Det er viktig at bildet har god nok oppløsning/kvalitet til at vi kan lese det. ',
+    },
+};
+
+const harIkkeVedleggDigitalAccordian: VedleggInnhold['accordians']['har_ikke_vedlegg_digital'] = {
+    har_ikke_vedlegg_digital: {
+        nb: 'Har du ikke vedleggene digitalt?',
+    },
+    slik_ta_bilde: {
+        nb: 'Slik tar du et godt bilde:',
+    },
+    instruksjoner: {
+        nb: [
+            'Hold mobilen eller kameraet direkte over dokumentet.',
+            'Dokumentet skal fylle hele bildet. Bildet skal ikke inneholde annen dokumentasjon eller gjenstander.',
+            'Bildet må inneholde all tekst i dokumentet. Hvis dokumentet er på mer enn èn side, bør du laste opp flere bilder.',
+        ],
+    },
+    ettet_tatt_bilde: {
+        nb: 'Etter at du har tatt bildet, sjekk følgende før du laster opp:',
+    },
+    instruksjoner_etter: {
+        nb: [
+            'Dokumentet har riktig retning.',
+            'Teksten til dokumentet er godt leselig.',
+            'Bildet er godt opplyst, uten skygger.',
+        ],
+    },
+};
 
 export const vedleggTekster: VedleggInnhold = {
     steg_tittel: {
@@ -18,5 +78,49 @@ export const vedleggTekster: VedleggInnhold = {
             'Har du ikke alle vedleggene i dag, kan du ettersende digitalt eller per post senest innen 14 dager.',
             'Vi kan ikke starte saksbehandlingen før vi har all dokumentasjon fra deg. Trenger du mer tid, kan du be om lenger frist på Ditt Nav etter at søknaden er sendt inn.',
         ],
+    },
+    du_må_legge_ved: {
+        nb: 'Ut fra det du har svart i søknaden, må du legge ved:',
+    },
+    samlet_faktura: {
+        nb: 'Har du samlet faktura for flere barn, trenger du bare å laste den opp en gang.',
+    },
+    faktura_lesmer: {
+        nb: {
+            header: 'Innhold i fakturaene',
+            innhold: [
+                'Fakturaen må inneholde barnets navn, beløp og perioden den gjelder for.',
+                'Vi dekker ikke mat eller bleier.',
+                'Vi godkjenner ikke skjermbilde av kontoutskrift, vipps eller lignende fordi vi trenger å se hva som er utgifter til pass av barn og hva som er utgift til bleier eller mat.',
+            ],
+        },
+    },
+    last_opp_faktura: {
+        nb: 'Last opp faktura',
+    },
+    last_opp_legeerklæring: {
+        nb: 'Last opp legeerklæring',
+    },
+    accordians: {
+        har_ikke_vedlegg_digital: harIkkeVedleggDigitalAccordian,
+        format_kvalitet: formatKvalitetAccordian,
+    },
+
+    // Hardkodet:
+    pass_ronja_espen: {
+        nb: 'Pass av Ronja og Espen',
+    },
+    vedlegg_espen_ronja: {
+        nb: [
+            'Avtale med privat barnepasser for Ronja',
+            'A-melding sendt Skatteetaten for barnepasser for Ronja',
+            'Faktura fra SFO/AKS for Espen',
+        ],
+    },
+    legeerklæring_espen_tittel: {
+        nb: 'Legeerklæring for Espen',
+    },
+    legeerklæring_espen: {
+        nb: 'Legerklæringen må inneholde opplysninger hvorfor Espen trenger ekstra pass. ',
     },
 };
