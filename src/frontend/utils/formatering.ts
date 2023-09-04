c75794652f3ead98523af368b3307b99a4fdc18a
Johan Blomgren <blommish@users.noreply.github.com>
2023-09-04 09:01:56 +0200

    Lagt til formattering av norsk dato (#70)

--- src/frontend/utils/formatering.ts
+++ src/frontend/utils/formatering.ts
@@ -1,5 +1,16 @@
+import { parseISO } from 'date-fns';
+
 import { Adresse } from '../typer/person';
+
+export const datoFormat = {
+    day: '2-digit',
+    month: '2-digit',
+    year: 'numeric',
+} as const;
 
 export const formaterAdresse = (adresse: Adresse) => {
     return `${adresse.adresse}, ${adresse.postnummer} ${adresse.poststed}`;
+};
+export const formaterIsoDato = (dato: string): string => {
+    return parseISO(dato).toLocaleDateString('no-NO', datoFormat);
 };
