0e770cd0ef9b5540e99c280a1aa4bf980ef119b9
solumstrand <144889236+solumstrand@users.noreply.github.com>
2023-09-13 14:37:27 +0200

    Tekster lagt inn i "Slik endrer du"
    Hvis mulig er det fint om det er lenker til skatteetaten og KRR: 
    - https://www.skatteetaten.no/person/folkeregister/flytte/endre-postadresse/ = melde adresseendring på Skattetatens sider
    - https://brukerprofil.difi.no/minprofil?locale=nb = Kontakt- og reservasjonsregisteret (KRR)

--- src/frontend/barnetilsyn/tekster/personalia.ts
+++ src/frontend/barnetilsyn/tekster/personalia.ts
@@ -21,19 +21,22 @@ export const personaliaTekster: PersonaliaInnhold = {
         nb: 'Vi har registrert dette om deg',
     },
     guide_innhold: {
-        nb: 'Det er lurt å sjekke om alt er riktig her så du får svar fra oss i tide og eventuelle utbetalinger kommer til rett konto.',
+        nb: 'Sjekk gjerne om alt er riktig her, så du får svar fra oss i tide og eventuelle utbetalinger kommer til rett konto.',
     },
     adresse_label: { nb: 'Folkeregistrert adresse' },
     adresse_lesmer: {
-        nb: { header: 'Slik endrer du folkeregistrert adresse', innhold: 'Innhold mangler' },
+        nb: { header: 'Slik endrer du folkeregistrert adresse', 
+             innhold: 'Vi henter adressen din fra folkeregisteret. Er noe feil, må du melde adresseendring på Skattetatens sider' },
     },
     telefonnr_label: { nb: 'Telefonnummer' },
     epost_label: { nb: 'E-post adresse' },
     tlf_epost_lesmer: {
-        nb: { header: 'Slik endrer du e-post eller telefonnummer', innhold: 'Innhold mangler' },
+        nb: { header: 'Slik endrer du e-post eller telefonnummer', 
+             innhold: 'Telefonnummer og e-postadressen er hentet fra Kontakt- og reservasjonsregisteret (KRR).' },
     },
     kontonr_label: { nb: 'Kontonummer' },
     kontonr_lesmer: {
-        nb: { header: 'Slik endrer du kontonummer', innhold: 'Innhold mangler' },
+        nb: { header: 'Slik endrer du kontonummer', 
+             innhold: 'Er kontonummeret feil, kan du endre det selv på Min side på Nav under "Personopplysninger".' },
     },
 };
