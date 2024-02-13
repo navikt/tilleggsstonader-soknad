9390d8d7acf41b32657f114e4a43634805a983ec
Johan Blomgren <blommish@users.noreply.github.com>
2024-02-13 08:26:18 +0100

    Setter auth header med cookieverdi då token-support ikke lengre støtter cookies (#193)

--- webpack/webpack.development.js
+++ webpack/webpack.development.js
@@ -1,3 +1,4 @@
+import cookieParser from 'cookie-parser';
 import path from 'path';
 import webpack from 'webpack';
 import { merge } from 'webpack-merge';
@@ -6,7 +7,7 @@ import common from './webpack.common.js';
 
 const publicPath = process.env.PUBLIC_URL || '/';
 
-console.log(`publicPath=${publicPath}`)
+console.log(`publicPath=${publicPath}`);
 
 const developmentConfig = merge(common, {
     mode: 'development',
@@ -20,8 +21,23 @@ const developmentConfig = merge(common, {
             index: publicPath,
         },
         proxy: {
-            '/api': 'http://localhost:8001'
-        }
+            '/api': {
+                target: 'http://localhost:8001',
+                onProxyReq: (proxyReq, req, res) => {
+                    const cookieValue = req.cookies['localhost-idtoken'];
+                    if (cookieValue) {
+                        proxyReq.setHeader('Authorization', `Bearer ${cookieValue}`);
+                    }
+                },
+            },
+        },
+        setupMiddlewares: (middlewares, devServer) => {
+            if (!devServer) {
+                throw new Error('webpack-dev-server is not defined');
+            }
+            devServer.app.use(cookieParser());
+            return middlewares;
+        },
     },
     output: {
         filename: '[name].bundle.js',
