57ed5b7287e88769e6f1848fec61442edde97634
sarahjelle <sara.hjelle@bekk.no>
2024-02-15 19:40:56 +0100

    Slett ubrukte tekster

--- src/frontend/barnetilsyn/tekster/vedlegg.ts
+++ src/frontend/barnetilsyn/tekster/vedlegg.ts
@@ -1,5 +1,5 @@
 import { Vedleggstype } from '../../typer/skjema';
-import { LesMer, TekstElement, Vedlegg } from '../../typer/tekst';
+import { TekstElement, Vedlegg } from '../../typer/tekst';
 
 export type TekstTypeVedlegg = {
     [key in Vedleggstype]: Vedlegg;
@@ -9,11 +9,6 @@ interface VedleggInnhold {
     steg_tittel: TekstElement<string>;
     innhold_tittel: TekstElement<string>;
     guide_innhold: TekstElement<string[]>;
-    du_må_legge_ved: TekstElement<string>;
-    samlet_faktura: TekstElement<string>;
-    faktura_lesmer: LesMer<string[]>;
-    last_opp_faktura: TekstElement<string>;
-    last_opp_legeerklæring: TekstElement<string>;
 
     accordians: {
         har_ikke_vedlegg_digital: {
@@ -78,27 +73,7 @@ export const vedleggTekster: VedleggInnhold = {
             'Har du ikke alle vedleggene i dag, kan du ettersende digitalt eller per post, senest innen 14 dager. Trenger du mer tid, kan du be om lenger frist på Min side på NAV.no etter at søknaden er sendt inn.',
         ],
     },
-    du_må_legge_ved: {
-        nb: 'Ut fra det du har svart i søknaden, må du legge ved:',
-    },
-    samlet_faktura: {
-        nb: 'Har du samlet faktura for flere barn, trenger du bare å laste den opp en gang.',
-    },
-    faktura_lesmer: {
-        header: { nb: 'Innhold i fakturaene' },
-        innhold: {
-            nb: [
-                'Fakturaen må inneholde barnets navn, beløp og perioden den gjelder for. Vi dekker ikke mat eller bleier.',
-                'Vi godkjenner ikke skjermbilde av kontoutskrift, vipps eller lignende fordi vi trenger å se hva som er utgifter til pass av barn og hva som er utgift til bleier eller mat.',
-            ],
-        },
-    },
-    last_opp_faktura: {
-        nb: 'Last opp faktura',
-    },
-    last_opp_legeerklæring: {
-        nb: 'Last opp legeerklæring',
-    },
+
     accordians: {
         har_ikke_vedlegg_digital: harIkkeVedleggDigitalAccordian,
         format_kvalitet: formatKvalitetAccordian,
