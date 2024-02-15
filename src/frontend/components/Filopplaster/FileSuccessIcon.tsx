81a863e6d42253bfe7dbcd8111c257beac885bde
sarahjelle <sara.hjelle@bekk.no>
2024-02-15 19:49:23 +0100

    Stor bokstav ikke "-" i react-komponent

--- src/frontend/components/Filopplaster/FileSuccessIcon.tsx
+++ src/frontend/components/Filopplaster/FileSuccessIcon.tsx
@@ -11,8 +11,8 @@ const FileSuccessIcon: React.FC = () => {
         >
             <rect width="40" height="40" rx="4" fill="#CCF1D6" />
             <path
-                fill-rule="evenodd"
-                clip-rule="evenodd"
+                fillRule="evenodd"
+                clipRule="evenodd"
                 d="M11.6665 10H22.0117L25.8332 13.8215V23.3333H24.1665V15.8333H19.9998V11.6667H13.3332V26.6667H19.1665V28.3333H11.6665V10ZM21.6665 12.0118L23.8213 14.1667H21.6665V12.0118ZM28.5573 21.9314L23.3596 27.5003L21.4659 25.291L20.2005 26.3757L23.3067 29.9997L29.7757 23.0686L28.5573 21.9314Z"
                 fill="#007C2E"
             />
