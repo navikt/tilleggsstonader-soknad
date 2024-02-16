d4c5828eb61a51c4402678affe2ddca2ff63f390
Runar Skagestad <runska@gmail.com>
2024-02-16 13:29:12 +0100

    Legg ut innholdet i vedleggsaccordions i en egen komponent

--- src/frontend/barnetilsyn/steg/6-vedlegg/Vedlegg.tsx
+++ src/frontend/barnetilsyn/steg/6-vedlegg/Vedlegg.tsx
@@ -1,15 +1,15 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 
 import { styled } from 'styled-components';
 
-import { Accordion, Heading, Label } from '@navikt/ds-react';
+import { Heading } from '@navikt/ds-react';
 
 import VedleggFelt from '../../../components/Filopplaster/VedleggFelt';
 import { PellePanel } from '../../../components/PellePanel/PellePanel';
 import Side from '../../../components/Side';
-import LocalePunktliste from '../../../components/Teksthåndtering/LocalePunktliste';
 import LocaleTekst from '../../../components/Teksthåndtering/LocaleTekst';
 import LocaleTekstAvsnitt from '../../../components/Teksthåndtering/LocaleTekstAvsnitt';
+import VedleggGenerellInfo from '../../../components/VedleggGenerellInfo';
 import { useSøknad } from '../../../context/SøknadContext';
 import { Dokument, DokumentasjonFelt } from '../../../typer/skjema';
 import { Stønadstype } from '../../../typer/stønadstyper';
@@ -64,54 +64,7 @@ const Vedlegg = () => {
                     </section>
                 ))}
             </VedleggContainer>
-            <Accordion>
-                <Accordion.Item>
-                    <Accordion.Header>
-                        <LocaleTekst
-                            tekst={
-                                vedleggTekster.accordians.har_ikke_vedlegg_digital
-                                    .har_ikke_vedlegg_digital
-                            }
-                        />
-                    </Accordion.Header>
-                    <Accordion.Content>
-                        <Label>
-                            <LocaleTekst
-                                tekst={
-                                    vedleggTekster.accordians.har_ikke_vedlegg_digital.slik_ta_bilde
-                                }
-                            />
-                        </Label>
-                        <LocalePunktliste
-                            innhold={
-                                vedleggTekster.accordians.har_ikke_vedlegg_digital.instruksjoner
-                            }
-                        />
-                        <Label>
-                            <LocaleTekst
-                                tekst={
-                                    vedleggTekster.accordians.har_ikke_vedlegg_digital
-                                        .ettet_tatt_bilde
-                                }
-                            />
-                        </Label>
-                        <LocalePunktliste
-                            innhold={
-                                vedleggTekster.accordians.har_ikke_vedlegg_digital
-                                    .instruksjoner_etter
-                            }
-                        />
-                    </Accordion.Content>
-                </Accordion.Item>
-                <Accordion.Item>
-                    <Accordion.Header>
-                        <LocaleTekst tekst={vedleggTekster.accordians.format_kvalitet.tittel} />
-                    </Accordion.Header>
-                    <Accordion.Content>
-                        <LocaleTekst tekst={vedleggTekster.accordians.format_kvalitet.innhold} />
-                    </Accordion.Content>
-                </Accordion.Item>
-            </Accordion>
+            <VedleggGenerellInfo />
         </Side>
     );
 };
