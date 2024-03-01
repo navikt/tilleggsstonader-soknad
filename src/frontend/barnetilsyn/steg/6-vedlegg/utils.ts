151b4cf9913e0b1a34d187189d60b0c9d374cc3b
Johan Blomgren <blommish@users.noreply.github.com>
2024-03-01 07:56:58 +0100

    Skal ikke lengre eksportere denne

--- src/frontend/barnetilsyn/steg/6-vedlegg/utils.ts
+++ src/frontend/barnetilsyn/steg/6-vedlegg/utils.ts
@@ -56,5 +56,5 @@ export const toggleHarSendtInn = (
  * Sjekker at det er samme type og barnId
  * Listen over dokumentajonFelt bør kun inneholde unike typer, eller unike typer/barnId
  */
-export const dokumentajonFeltEquals = (first: DokumentasjonFelt, second: DokumentasjonFelt) =>
+const dokumentajonFeltEquals = (first: DokumentasjonFelt, second: DokumentasjonFelt) =>
     first.type === second.type && first.barnId === second.barnId;
