3639f4b2fe1ae2705d17906bf8abb9fcbe90a015
Johan Blomgren <blommish@users.noreply.github.com>
2024-03-26 16:29:58 +0100

    Lagt til MottarDuPengestøtte

--- src/frontend/barnetilsyn/steg/2-hovedytelse/ArbeidOgOpphold/util.ts
+++ src/frontend/barnetilsyn/steg/2-hovedytelse/ArbeidOgOpphold/util.ts
@@ -1,4 +1,23 @@
-import { ArbeidOgOpphold } from '../../../../typer/søknad';
+import { EnumFlereValgFelt } from '../../../../typer/skjema';
+import { ArbeidOgOpphold, MottarPengestøtteTyper } from '../../../../typer/søknad';
+import { harVerdi } from '../../../../utils/typer';
 
 export const skalTaStillingTilLandForJobberIAnnetLand = (opphold: ArbeidOgOpphold): boolean =>
     opphold.jobberIAnnetLandEnnNorge?.verdi === 'JA';
+
+export const skalTaStillingTilPengestøtte = (opphold: ArbeidOgOpphold): boolean =>
+    opphold?.jobberIAnnetLandEnnNorge?.verdi === 'NEI' ||
+    harVerdi(opphold.hvilketLandJobberIAnnetLandEnnNorge?.verdi);
+
+const mottarPengestøtteTyperSomMåSåTaStillingTilLand: MottarPengestøtteTyper[] = [
+    'SYKEPENGER',
+    'PENSJON',
+    'ANNEN_PENGESTØTTE',
+];
+
+export const skalTaStillingTilLandForPengestøtte = (
+    verdier: EnumFlereValgFelt<MottarPengestøtteTyper> | undefined
+): boolean =>
+    (verdier?.verdier || []).some(
+        (verdi) => mottarPengestøtteTyperSomMåSåTaStillingTilLand.indexOf(verdi.verdi) > -1
+    );
