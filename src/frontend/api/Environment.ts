ec37f9eee26926cd86017d24c0ccf5288427854d
Johan Blomgren <blommish@users.noreply.github.com>
2024-04-18 13:48:09 +0200

    Oppdatert lenke til nytt fyll-ut-skjema

--- src/frontend/api/Environment.ts
+++ src/frontend/api/Environment.ts
@@ -23,7 +23,8 @@ const Environment = (): EnvironmentProps => {
             wonderwallUrl:
                 'https://tilleggsstonader.ekstern.dev.nav.no/tilleggsstonader/soknad/oauth2/login?redirect=',
             sentryUrl: 'https://06b839ad5487467cb88097c5a27bbbb5@sentry.gc.nav.no/167',
-            urlGammelSøknad: 'https://soknad.intern.dev.nav.no/soknadtilleggsstonader',
+            urlGammelSøknad:
+                'https://skjemadelingslenke.ekstern.dev.nav.no/fyllut/nav111215b?sub=digital',
             miljø: 'preprod',
             modellVersjon: modellVersjon,
         };
@@ -33,7 +34,7 @@ const Environment = (): EnvironmentProps => {
             vedleggProxyUrl: 'https://www.nav.no/tilleggsstonader/soknad/api/vedlegg',
             wonderwallUrl: 'https://www.nav.no/tilleggsstonader/soknad/oauth2/login?redirect=',
             sentryUrl: 'https://06b839ad5487467cb88097c5a27bbbb5@sentry.gc.nav.no/167',
-            urlGammelSøknad: 'https://tjenester.nav.no/soknadtilleggsstonader',
+            urlGammelSøknad: 'https://www.nav.no/fyllut/nav111215b?sub=digital',
             miljø: 'production',
             modellVersjon: modellVersjon,
         };
