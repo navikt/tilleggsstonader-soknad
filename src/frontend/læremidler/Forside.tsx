2b32e6aebb75b29d90dd43e95352051238ba47dc
sarahjelle <sara.hjelle@bekk.no>
2024-08-27 13:01:51 +0200

    Kopier forside fra barnetilsyn

---
+++ src/frontend/læremidler/Forside.tsx
@@ -0,0 +1,172 @@
+import { useEffect, useState } from 'react';
+
+import { useLocation, useNavigate } from 'react-router';
+import { styled } from 'styled-components';
+
+import {
+    Accordion,
+    Alert,
+    BodyShort,
+    Button,
+    Checkbox,
+    CheckboxGroup,
+    Heading,
+    Label,
+} from '@navikt/ds-react';
+
+import { forsideTekster } from './tekster/forside';
+import { loggAccordionEvent, loggBesøkBarnetilsyn, loggSkjemaStartet } from '../api/amplitude';
+import { RoutesBarnetilsyn, ERouteBarnetilsyn } from '../barnetilsyn/routing/routesBarnetilsyn';
+import { PellePanel } from '../components/PellePanel/PellePanel';
+import { Container } from '../components/Side';
+import LocaleInlineLenke from '../components/Teksthåndtering/LocaleInlineLenke';
+import LocalePunktliste from '../components/Teksthåndtering/LocalePunktliste';
+import LocaleTekst from '../components/Teksthåndtering/LocaleTekst';
+import LocaleTekstAvsnitt from '../components/Teksthåndtering/LocaleTekstAvsnitt';
+import { usePassAvBarnSøknad } from '../context/PassAvBarnSøknadContext';
+import { usePerson } from '../context/PersonContext';
+import { fellesTekster } from '../tekster/felles';
+import { Stønadstype } from '../typer/stønadstyper';
+import { erSnartNyttSkoleår } from '../utils/dato';
+import { hentNesteRoute } from '../utils/routes';
+
+const KnappeContainer = styled(BodyShort)`
+    display: flex;
+    gap: 1rem;
+    justify-content: center;
+`;
+
+const Forside: React.FC = () => {
+    const navigate = useNavigate();
+    const location = useLocation();
+    const { harBekreftet, settHarBekreftet } = usePassAvBarnSøknad();
+    const { person } = usePerson();
+
+    const [skalViseFeilmelding, settSkalViseFeilmelding] = useState(false);
+
+    useEffect(() => {
+        const route = RoutesBarnetilsyn[0];
+        loggBesøkBarnetilsyn(route.path, route.label);
+    }, []);
+
+    const startSøknad = () => {
+        if (harBekreftet) {
+            loggSkjemaStartet(Stønadstype.BARNETILSYN);
+            const nesteRoute = hentNesteRoute(RoutesBarnetilsyn, location.pathname);
+            navigate(nesteRoute.path);
+        } else {
+            settSkalViseFeilmelding(true);
+        }
+    };
+
+    const loggAccordionÅpning = (skalÅpne: boolean, tittel: string) => {
+        loggAccordionEvent(skalÅpne, tittel, ERouteBarnetilsyn.FORSIDE);
+    };
+
+    return (
+        <Container>
+            <PellePanel poster>
+                <Label>
+                    <LocaleTekst
+                        tekst={forsideTekster.veileder_tittel}
+                        argument0={person.fornavn}
+                    />
+                </Label>
+                <LocaleTekstAvsnitt tekst={forsideTekster.veileder_innhold} />
+            </PellePanel>
+            {erSnartNyttSkoleår() && (
+                <Alert variant="info">
+                    <Heading size="small" spacing>
+                        <LocaleTekst tekst={forsideTekster.mottatt_faktura_alert_tittel} />
+                    </Heading>
+                    <LocaleTekstAvsnitt tekst={forsideTekster.mottatt_faktura_alert_innhold} />
+                </Alert>
+            )}
+            <LocalePunktliste
+                tittel={forsideTekster.dine_plikter_tittel}
+                innhold={forsideTekster.dine_plikter_innhold}
+            />
+            <Accordion>
+                <Accordion.Item
+                    onOpenChange={(skalÅpne) =>
+                        loggAccordionÅpning(skalÅpne, 'Hvilke utgifter dekker vi?')
+                    }
+                >
+                    <Accordion.Header>
+                        <LocaleTekst tekst={forsideTekster.utgifter_som_dekkes_tittel} />
+                    </Accordion.Header>
+                    <Accordion.Content>
+                        <LocaleTekstAvsnitt tekst={forsideTekster.utgifter_som_dekkes_innhold} />
+                    </Accordion.Content>
+                </Accordion.Item>
+                <Accordion.Item
+                    onOpenChange={(skalÅpne) =>
+                        loggAccordionÅpning(skalÅpne, 'Dokumentasjon av utgifter')
+                    }
+                >
+                    <Accordion.Header>
+                        <LocaleTekst tekst={forsideTekster.dokumentasjon_utgifter_tittel} />
+                    </Accordion.Header>
+                    <Accordion.Content>
+                        {forsideTekster.dokumentasjon_utgifter_innhold.map((tekst, indeks) => (
+                            <LocalePunktliste
+                                key={indeks}
+                                tittel={tekst.tittel}
+                                innhold={tekst.innhold}
+                                tittelSomLabel
+                            />
+                        ))}
+                    </Accordion.Content>
+                </Accordion.Item>
+                <Accordion.Item
+                    onOpenChange={(skalÅpne) =>
+                        loggAccordionÅpning(skalÅpne, 'Informasjon vi henter')
+                    }
+                >
+                    <Accordion.Header>
+                        <LocaleTekst tekst={forsideTekster.info_som_hentes_tittel} />
+                    </Accordion.Header>
+                    <Accordion.Content>
+                        <LocalePunktliste
+                            tittel={forsideTekster.info_som_hentes_innhold1}
+                            innhold={forsideTekster.info_som_hentes_innhold2}
+                            tittelSomLabel
+                        />
+                        <LocalePunktliste
+                            tittel={forsideTekster.info_som_hentes_innhold3}
+                            innhold={forsideTekster.info_som_hentes_innhold4}
+                            tittelSomLabel
+                        />
+                        <LocaleInlineLenke tekst={forsideTekster.info_som_hentes_innhold5} />
+                    </Accordion.Content>
+                </Accordion.Item>
+            </Accordion>
+            <CheckboxGroup
+                legend={<LocaleTekst tekst={fellesTekster.vi_stoler_tittel} />}
+                error={
+                    skalViseFeilmelding && (
+                        <LocaleTekst tekst={fellesTekster.vi_stoler_feilmelding} />
+                    )
+                }
+                value={[harBekreftet]}
+            >
+                <Checkbox
+                    value={true}
+                    onChange={(e) => {
+                        settHarBekreftet(e.target.checked);
+                        settSkalViseFeilmelding(false);
+                    }}
+                >
+                    <LocaleTekst tekst={fellesTekster.vi_stoler_innhold} />
+                </Checkbox>
+            </CheckboxGroup>
+            <KnappeContainer>
+                <Button onClick={startSøknad} variant="primary">
+                    Start søknad
+                </Button>
+            </KnappeContainer>
+        </Container>
+    );
+};
+
+export default Forside;
---
+++ src/frontend/læremidler/tekster/forside.ts
@@ -0,0 +1,125 @@
+import { TekstElement, InlineLenke, Punktliste } from '../../typer/tekst';
+
+interface ForsideInnhold {
+    veileder_tittel: TekstElement<string>;
+    veileder_innhold: TekstElement<string[]>;
+    mottatt_faktura_alert_tittel: TekstElement<string>;
+    mottatt_faktura_alert_innhold: TekstElement<string[]>;
+    dine_plikter_tittel: TekstElement<string>;
+    dine_plikter_innhold: TekstElement<string[]>;
+    utgifter_som_dekkes_tittel: TekstElement<string>;
+    utgifter_som_dekkes_innhold: TekstElement<string[]>;
+    info_som_hentes_tittel: TekstElement<string>;
+    info_som_hentes_innhold1: TekstElement<string>;
+    info_som_hentes_innhold2: TekstElement<string[]>;
+    info_som_hentes_innhold3: TekstElement<string>;
+    info_som_hentes_innhold4: TekstElement<string[]>;
+    info_som_hentes_innhold5: TekstElement<InlineLenke>;
+    dokumentasjon_utgifter_tittel: TekstElement<string>;
+    dokumentasjon_utgifter_innhold: Punktliste[];
+}
+
+export const forsideTekster: ForsideInnhold = {
+    veileder_tittel: {
+        nb: 'Hei [0]!',
+    },
+    veileder_innhold: {
+        nb: [
+            'Denne pengestøtten kan gis til deg som gjennomfører en arbeidsrettet aktivitet og er enslig mor/far, gjenlevende, mottar AAP, uføretrygd eller har nedsatt arbeidsevne.',
+        ],
+    },
+    mottatt_faktura_alert_tittel: {
+        nb: 'Skal du søke for nytt skole/barnehageår?',
+    },
+    mottatt_faktura_alert_innhold: {
+        nb: [
+            'Du må legge ved faktura for perioden du søker for. Det vil ta lengre tid å få svar fra oss hvis vi må hente inn fra deg i etterkant.',
+            'Hvis du ikke har mottatt fakturaen ennå, anbefaler vi at du venter med å søke til du får den.',
+        ],
+    },
+    dine_plikter_tittel: {
+        nb: 'Viktig å vite før du søker',
+    },
+    dine_plikter_innhold: {
+        nb: [
+            'Du må gi oss riktige opplysninger i søknaden.',
+            'Du må dokumentere utgiftene dine i søknaden.',
+            'Du må gi beskjed til oss hvis situasjonen din endrer seg.',
+            'Hvis du får penger du ikke har rett på, kan vi kreve at du betaler de tilbake.',
+        ],
+    },
+    utgifter_som_dekkes_tittel: {
+        nb: 'Hvilke utgifter dekker vi?',
+    },
+    //TODO: Fiks med lenke og mellomrom
+    utgifter_som_dekkes_innhold: {
+        nb: [
+            'Vi dekker opp til 64 prosent av utgiftene du har til pass av barn.',
+            'Utgifter til mat og bleier dekkes ikke.',
+            'Som hovedregel gis det bare støtte til pass av barn til og med 4. klasse.',
+        ],
+    },
+    info_som_hentes_tittel: {
+        nb: 'Informasjon vi henter om deg',
+    },
+    info_som_hentes_innhold1: {
+        nb: 'I tillegg til den informasjonen du oppgir i søknaden, henter vi:',
+    },
+    info_som_hentes_innhold2: {
+        nb: [
+            'adressen din og opplysninger om dine barn fra Folkeregisteret',
+            'informasjon om arbeidsrettet aktivitet',
+            'hvilke andre andre ytelser du mottar fra NAV',
+        ],
+    },
+    info_som_hentes_innhold3: {
+        nb: 'Ved behov sjekker vi:',
+    },
+    info_som_hentes_innhold4: {
+        nb: [
+            'om du har barn det utbetales kontantstøtte for',
+            'om du sender meldekort',
+            'om du er medlem i folketrygden',
+        ],
+    },
+    info_som_hentes_innhold5: {
+        nb: [
+            'NAV er ansvarlig for å behandle personopplysningene dine. Vi deler ikke informasjonen du gir oss i søknaden med noen andre.  ',
+            {
+                tekst: 'Personvernerklæringen på nav.no (åpnes i ny fane)',
+                url: 'https://www.nav.no/personvernerklaering',
+                variant: 'neutral',
+            },
+            ' gir mer informasjon om hvordan vi behandler dine personopplysninger.',
+        ],
+    },
+    dokumentasjon_utgifter_tittel: {
+        nb: 'Dokumentasjon av utgifter',
+    },
+    dokumentasjon_utgifter_innhold: [
+        {
+            tittel: {
+                nb: 'For barn i barnehage, SFO eller AKS legger du ved faktura som inneholder:',
+            },
+            innhold: {
+                nb: [
+                    'barnets navn',
+                    'hva som er utgifter til pass, mat og eventuelt bleier',
+                    'perioden utgiftene gjelder for',
+                ],
+            },
+        },
+        {
+            tittel: {
+                nb: 'Har du privat pass som f.eks. dagmamma eller praktikant legger du ved:',
+            },
+            innhold: {
+                nb: [
+                    'avtale med barnepasser',
+                    'kvittering for betaling',
+                    'eventuelt A-melding sendt Skatteetaten',
+                ],
+            },
+        },
+    ],
+};
