72dbdc95742082d17ef377264b63cbee28c7af1b
sarahjelle <sara.hjelle@bekk.no>
2024-08-27 18:24:52 +0200

    Oppdater id for feilmelding

--- src/frontend/læremidler/steg/2-utdanning/validering.ts
+++ src/frontend/læremidler/steg/2-utdanning/validering.ts
@@ -5,7 +5,7 @@ import { utdanningTekster } from '../../tekster/utdanning';
 export const feilAnnenUtdanning = (feil: Valideringsfeil, locale: Locale) => ({
     ...feil,
     annenUtdanning: {
-        id: '3',
+        id: '1', // TODO: Oppdater når vi hentet utdanning
         melding: utdanningTekster.radio_annen_utdanning_feilmelding[locale],
     },
 });
