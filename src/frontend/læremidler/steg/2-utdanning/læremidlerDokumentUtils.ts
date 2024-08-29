d2275706eb0c6fcf299670225727992a585942fa
sarahjelle <sara.hjelle@bekk.no>
2024-08-29 11:29:53 +0200

    Legg ved dokumentasjonsbehov dersom bruker har funksjonsnedsettelse

---
+++ src/frontend/læremidler/steg/2-utdanning/læremidlerDokumentUtils.ts
@@ -0,0 +1,11 @@
+import { Dokumentasjonsbehov, EnumFelt, VedleggstypeLæremidler } from '../../../typer/skjema';
+import { JaNei } from '../../../typer/søknad';
+
+export const finnDokumentasjonsbehov = (
+    harFunksjonsnedsettelse: EnumFelt<JaNei> | undefined
+): Dokumentasjonsbehov[] => {
+    if (harFunksjonsnedsettelse && harFunksjonsnedsettelse.verdi === 'JA') {
+        return [{ type: VedleggstypeLæremidler.DOKUMENTASJON_FUNKSJONSNEDSETTELSE }];
+    }
+    return [];
+};
