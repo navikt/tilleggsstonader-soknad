907e3aded8f1ffb34befe680abd9c152092c0069
sarahjelle <sara.hjelle@bekk.no>
2024-08-30 10:07:00 +0200

    Skal kun vise svar dersom svar er nei

--- src/frontend/components/Oppsummering/Hovedytelse/Opphold.tsx
+++ src/frontend/components/Oppsummering/Hovedytelse/Opphold.tsx
@@ -22,44 +22,50 @@ const OppholdOppsummering: React.FC<{
             <FormSummary.Label>
                 <LocaleTekst tekst={tittel} />
             </FormSummary.Label>
-
-            <FormSummary.Answers className="Blue">
-                {opphold.map((opphold, indeks) => (
-                    <FormSummary.Answer key={indeks}>
-                        <FormSummary.Label>
-                            {opphold.land && (
-                                <FormSummary.Answer>
-                                    <FormSummary.Label>
-                                        <LocaleTekst tekst={fellesTekster.land} />
-                                    </FormSummary.Label>
-                                    <FormSummary.Value>{opphold.land.svarTekst}</FormSummary.Value>
-                                </FormSummary.Answer>
-                            )}
-                            {opphold.årsak && (
-                                <FormSummary.Answer>
-                                    <FormSummary.Label>
-                                        <LocaleTekst tekst={fellesTekster.årsak} />
-                                    </FormSummary.Label>
-                                    <FormSummary.Value>
-                                        {flervalgTilKommaStreng(opphold.årsak)}
-                                    </FormSummary.Value>
-                                </FormSummary.Answer>
-                            )}
-                            {opphold.fom && opphold.tom && (
-                                <FormSummary.Answer>
-                                    <FormSummary.Label>
-                                        <LocaleTekst tekst={fellesTekster.periode} />
-                                    </FormSummary.Label>
-                                    <FormSummary.Value>
-                                        {formaterIsoDato(opphold.fom.verdi)} -{' '}
-                                        {formaterIsoDato(opphold.tom.verdi)}
-                                    </FormSummary.Value>
-                                </FormSummary.Answer>
-                            )}
-                        </FormSummary.Label>
-                    </FormSummary.Answer>
-                ))}
-            </FormSummary.Answers>
+            {harOppholdSvar.verdi === 'NEI' && (
+                <FormSummary.Value>{harOppholdSvar.svarTekst}</FormSummary.Value>
+            )}
+            {opphold.length > 0 && (
+                <FormSummary.Answers className="Blue">
+                    {opphold.map((opphold, indeks) => (
+                        <FormSummary.Answer key={indeks}>
+                            <FormSummary.Label>
+                                {opphold.land && (
+                                    <FormSummary.Answer>
+                                        <FormSummary.Label>
+                                            <LocaleTekst tekst={fellesTekster.land} />
+                                        </FormSummary.Label>
+                                        <FormSummary.Value>
+                                            {opphold.land.svarTekst}
+                                        </FormSummary.Value>
+                                    </FormSummary.Answer>
+                                )}
+                                {opphold.årsak && (
+                                    <FormSummary.Answer>
+                                        <FormSummary.Label>
+                                            <LocaleTekst tekst={fellesTekster.årsak} />
+                                        </FormSummary.Label>
+                                        <FormSummary.Value>
+                                            {flervalgTilKommaStreng(opphold.årsak)}
+                                        </FormSummary.Value>
+                                    </FormSummary.Answer>
+                                )}
+                                {opphold.fom && opphold.tom && (
+                                    <FormSummary.Answer>
+                                        <FormSummary.Label>
+                                            <LocaleTekst tekst={fellesTekster.periode} />
+                                        </FormSummary.Label>
+                                        <FormSummary.Value>
+                                            {formaterIsoDato(opphold.fom.verdi)} -{' '}
+                                            {formaterIsoDato(opphold.tom.verdi)}
+                                        </FormSummary.Value>
+                                    </FormSummary.Answer>
+                                )}
+                            </FormSummary.Label>
+                        </FormSummary.Answer>
+                    ))}
+                </FormSummary.Answers>
+            )}
         </FormSummary.Answer>
     );
 };
