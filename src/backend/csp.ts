1a1ac8e078aca515d0844ad7f54a2b317e2d0c2e
Runar Skagestad <runar.skagestad@nav.no>
2024-11-11 18:02:39 +0100

    🔒 Legg inn CSP-header. Veldig mange av direktivene kreves av dekoratøren; disse er markert.
    Foreløpig blir ingenting blokkert. Tanken er å fjerne 'REPORT-ONLY' etter en ukes tid.

---
+++ src/backend/csp.ts
@@ -0,0 +1,132 @@
+export const cspMap = (): Record<string, string[]> => {
+    return {
+        // Hvor vi får hente ressurser fra, hvis ingen andre spesifikke regler gjelder
+        'default-src': ["'self'", '*.nav.no'],
+
+        // Hvor vi får hente js-filer fra
+        'script-src': [
+            "'self'",
+            // ┌────────── Dekoratøren ──────────┐
+            '*.nav.no',
+            'uxsignals-frontend.uxsignals.app.iterate.no',
+            '*.psplugin.com',
+            '*.taskanalytics.com',
+            '*.hotjar.com',
+            'nav.boost.ai',
+            "'unsafe-inline'",
+            "'unsafe-eval'",
+            // └────────────────────────────────┘
+        ],
+
+        // Hvor vi får hente <script>-elementer fra
+        'script-src-elem': [
+            "'self'",
+            // ┌────────── Dekoratøren ──────────┐
+            '*.nav.no',
+            'uxsignals-frontend.uxsignals.app.iterate.no',
+            '*.psplugin.com',
+            '*.hotjar.com',
+            '*.taskanalytics.com',
+            'nav.boost.ai',
+            "'unsafe-inline'",
+            // └────────────────────────────────┘
+        ],
+
+        // Hvor vi får hente css-filer fra
+        'style-src': [
+            "'self'",
+            // ┌────────── Dekoratøren ──────────┐
+            '*.nav.no',
+            '*.psplugin.com',
+            "'unsafe-inline'",
+            '*.googleapis.com',
+            '*.gstatic.com',
+            // └────────────────────────────────┘
+        ],
+
+        // Hvor vi får hente <style>-elementer fra
+        'style-src-elem': [
+            // ┌────────── Dekoratøren ──────────┐
+            '*.nav.no',
+            '*.psplugin.com',
+            "'unsafe-inline'",
+            '*.googleapis.com',
+            '*.gstatic.com',
+            // └────────────────────────────────┘
+        ],
+
+        // Hvor vi får kjøre XHR/REST-kall mot
+        'connect-src': [
+            "'self'",
+            'm3pb011r.apicdn.sanity.io',
+            'amplitude.nav.no',
+            // ┌────────── Dekoratøren ──────────┐
+            '*.nav.no',
+            'api.uxsignals.com',
+            '*.boost.ai',
+            '*.psplugin.com',
+            '*.hotjar.com',
+            '*.hotjar.io',
+            '*.taskanalytics.com',
+            // └────────────────────────────────┘
+        ],
+
+        // Hvor vi får sende inn forms
+        'form-action': ["'self'"],
+
+        // Hvor fonter kan hentes fra
+        'font-src': [
+            "'self'",
+            // ┌────────── Dekoratøren ──────────┐
+            '*.psplugin.com',
+            '*.hotjar.com',
+            'cdn.nav.no',
+            '*.googleapis.com',
+            '*.gstatic.com',
+            'data:',
+            // └────────────────────────────────┘
+        ],
+
+        // Hvor vi hente innhold til iFrames fra
+        'frame-src': [
+            // ┌────────── Dekoratøren ──────────┐
+            '*.hotjar.com',
+            'video.qbrick.com',
+            'player.vimeo.com',
+            '*.nav.no',
+            // └────────────────────────────────┘
+        ],
+
+        // Hvor bilder kan hentes fra
+        'img-src': [
+            "'self'",
+            'data:',
+            // ┌────────── Dekoratøren ──────────┐
+            '*.nav.no',
+            'uxsignals-frontend.uxsignals.app.iterate.no',
+            '*.psplugin.com',
+            '*.vimeocdn.com',
+            '*.hotjar.com',
+            'www.vergic.com',
+            // └────────────────────────────────┘
+        ],
+
+        // Hvor web workers kan kjøres fra
+        'worker-src': ['*.nav.no', 'blob:'],
+
+        // Hvor child-browsing-kontekster kan kjøres fra, som iFrames eller pop-ups
+        'child-src': ['*.nav.no', 'blob:'],
+
+        // Hvor manifest-filer kan hentes fra
+        'manifest-src': ["'self'", 'www.nav.no', 'oidc-ver2.difi.no', 'idporten-ver2.difi.no'],
+
+        // Hvor denne siden kan integreres (f.eks. iFrames)
+        'frame-ancestors': ["'self'", '*.psplugin.com'],
+    };
+};
+
+export const cspString = () => {
+    return Object.entries(cspMap())
+        .map((entry) => `${entry[0]} ${entry[1].join(' ')}`)
+        .join('; ');
+};
--- src/backend/server.ts
+++ src/backend/server.ts
@@ -1,12 +1,21 @@
 import cookieParser from 'cookie-parser';
 import express from 'express';
 
+import { cspString } from './csp';
 import routes from './routes';
+
 const app = express();
 
 if (process.env.ENV === 'localhost') {
     app.use(cookieParser());
 }
+
+app.use((_req, res, next) => {
+    // TODO: Bytt til 'Content-Security-Policy' etter at vi har undersøkt loggene etter en ukes tid
+    // Vi bør ikke se noen logginnslag av typen "[Report Only] Refused to load <some resource> because..."
+    res.header('Content-Security-Policy-Report-Only', cspString());
+    next();
+});
 
 app.use(routes());
 
