8ffc04ed4f067a4a8b59e9728315961cc526e56c
Runar Skagestad <runar.skagestad@nav.no>
2024-11-19 14:48:50 +0100

    Tillat henting av bilder, men blokker manifester som test

--- src/backend/csp.ts
+++ src/backend/csp.ts
@@ -1,4 +1,43 @@
-export const cspMap = (): Record<string, string[]> => {
+import bodyParser from 'body-parser';
+import express, { Express } from 'express';
+
+import logger from './logger';
+import { miljø } from './miljø';
+
+const BASE_PATH = '/tilleggsstonader';
+const BASE_PATH_SOKNAD = `${BASE_PATH}/soknad`;
+
+export function csp(): Express {
+    const app = express();
+
+    const rapporteringsendepunkt = `${miljø.internalUrl}/csp-violation`;
+
+    app.use((_req, res, next) => {
+        // TODO: Fjern '-Report-Only' etter at vi har undersøkt loggene etter en ukes tid
+        res.header(
+            'Content-Security-Policy-Report-Only',
+            cspString() + '; report-to csp-violation-report'
+        );
+        res.header('Reporting-Endpoints', `csp-endpoint="${rapporteringsendepunkt}"`);
+
+        next();
+    });
+
+    app.use(bodyParser.json());
+    app.post(`${BASE_PATH_SOKNAD}/internal/csp-violation`, (req, res) => {
+        const cspReport = req.body['csp-report'];
+
+        if (cspReport) {
+            logger.warning('CSP Violation:', cspReport);
+        } else {
+            logger.error('Received a malformed CSP violation report.');
+        }
+        res.status(204).end();
+    });
+    return app;
+}
+
+const cspMap = (): Record<string, string[]> => {
     return {
         // Hvor vi får hente ressurser fra, hvis ingen andre spesifikke regler gjelder
         'default-src': ["'self'", '*.nav.no'],
@@ -125,7 +164,7 @@ export const cspMap = (): Record<string, string[]> => {
     };
 };
 
-export const cspString = () => {
+const cspString = () => {
     return Object.entries(cspMap())
         .map((entry) => `${entry[0]} ${entry[1].join(' ')}`)
         .join('; ');
--- src/backend/server.ts
+++ src/backend/server.ts
@@ -1,8 +1,7 @@
-import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
 import express from 'express';
 
-import { cspString } from './csp';
+import { csp } from './csp';
 import logger from './logger';
 import routes from './routes';
 
@@ -11,28 +10,9 @@ const app = express();
 if (process.env.ENV === 'localhost') {
     app.use(cookieParser());
 }
-
-app.use((_req, res, next) => {
-    // TODO: Fjern '-Report-Only' etter at vi har undersøkt loggene etter en ukes tid
-    res.header(
-        'Content-Security-Policy-Report-Only',
-        cspString() + '; report-uri /csp-violation-report'
-    );
-    next();
-});
-
-app.use(bodyParser.json());
-app.post('/csp-violation-report', (req, res) => {
-    const cspReport = req.body['csp-report'];
-
-    if (cspReport) {
-        logger.warning('CSP Violation:', cspReport);
-    } else {
-        logger.error('Received a malformed CSP violation report.');
-    }
-    res.status(204).end();
-});
-
 app.use(routes());
+app.use(csp());
+
+logger.info('Starter server på port 3000');
 
 app.listen(3000);
