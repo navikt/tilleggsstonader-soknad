9ac76c080c8ac0a944e3d108e74d8f6317365493
Runar Skagestad <runar.skagestad@nav.no>
2024-11-19 11:59:21 +0100

    Log CSP-violations

--- src/backend/server.ts
+++ src/backend/server.ts
@@ -1,7 +1,9 @@
+import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
 import express from 'express';
 
 import { cspString } from './csp';
+import logger from './logger';
 import routes from './routes';
 
 const app = express();
@@ -11,10 +13,24 @@ if (process.env.ENV === 'localhost') {
 }
 
 app.use((_req, res, next) => {
-    // TODO: Bytt til 'Content-Security-Policy' etter at vi har undersøkt loggene etter en ukes tid
-    // Vi bør ikke se noen logginnslag av typen "[Report Only] Refused to load <some resource> because..."
-    res.header('Content-Security-Policy-Report-Only', cspString());
+    // TODO: Fjern '-Report-Only' etter at vi har undersøkt loggene etter en ukes tid
+    res.header(
+        'Content-Security-Policy-Report-Only',
+        cspString() + ' report-uri /csp-violation-report'
+    );
     next();
+});
+
+app.use(bodyParser.json());
+app.post('/csp-violation-report', (req, res) => {
+    const cspReport = req.body['csp-report'];
+
+    if (cspReport) {
+        logger.warning('CSP Violation:', cspReport);
+    } else {
+        logger.error('Received a malformed CSP violation report.');
+    }
+    res.status(204).end();
 });
 
 app.use(routes());
