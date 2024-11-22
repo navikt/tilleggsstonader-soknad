a3992e1598945436686f7677e47fdd5b59102e2b
Runar Skagestad <runar.skagestad@nav.no>
2024-11-22 12:52:23 +0100

    Logg ut rapporteringsendepunktet

--- src/backend/csp.ts
+++ src/backend/csp.ts
@@ -8,7 +8,9 @@ const rapporteringsendepunkt = `${miljø.reportingUrl}/csp-violation`;
 export function cspDirective() {
     return async (req: Request, res: Response, next: NextFunction) => {
         // TODO: Fjern '-Report-Only' etter at vi har undersøkt loggene etter en ukes tid
-        logger.info(`Legger til CSP-header på responsen til ${req.url}`);
+        logger.info(
+            `Legger til CSP-header på responsen til ${req.url} med report-to=${rapporteringsendepunkt}`
+        );
         res.header(
             'Content-Security-Policy-Report-Only',
             cspString() + '; report-to csp-violation'
