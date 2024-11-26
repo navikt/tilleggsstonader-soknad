dc7228f245f4d2f772cd0040be7917d3dc1f55c4
Sigmund Berbom <sberbom@gmail.com>
2024-11-26 13:15:10 +0100

    Oppdater feilmelding tekster

--- src/frontend/tekster/filopplasting.ts
+++ src/frontend/tekster/filopplasting.ts
@@ -2,20 +2,6 @@ import { FeilmeldingerFraDokument } from '../components/Filopplaster/feilmelding
 import { MAKS_FILSTØRRELSE_FORMATTERT } from '../components/Filopplaster/utils';
 import { TekstElement } from '../typer/tekst';
 
-export interface FilopplastingInnhold {
-    last_opp_fil_knapp: TekstElement<string>;
-    krav_dokumentasjon_overskrift: TekstElement<string>;
-}
-
-export const filopplastingTekster: FilopplastingInnhold = {
-    last_opp_fil_knapp: {
-        nb: 'Last opp fil',
-    },
-    krav_dokumentasjon_overskrift: {
-        nb: 'Krav til dokumentasjonen',
-    },
-};
-
 export const teksterFeilmeldinger: {
     enFil: TekstElement<string>;
     maksstørrelse: TekstElement<string>;
@@ -29,21 +15,21 @@ export const teksterFeilmeldinger: {
         nb: `Må laste opp en og en fil`,
     },
     maksstørrelse: {
-        nb: `"[0]" er for stor (maksimal filstørrelse er ${MAKS_FILSTØRRELSE_FORMATTERT}).`,
+        nb: `For stor (maksimal filstørrelse er ${MAKS_FILSTØRRELSE_FORMATTERT}).`,
     },
     filtype: {
-        nb: '"[0]" – Ugyldig filtype.',
+        nb: 'Ugyldig filtype.',
     },
     feilmeldinger: {
         generisk: {
-            nb: 'Feilet opplasting av "[0]". ', // Space på slutten fordi det kommer en mer spesifik feilmelding etterpå
+            nb: 'Feilet opplasting. ', // Space på slutten fordi det kommer en mer spesifik feilmelding etterpå
         },
         fra_dokument: {
             IMAGE_DIMENSIONS_TOO_SMALL: {
                 nb: 'Bilde du har forsøkt å laste opp er for lite. Bilde må være større enn 400x400 piksler.',
             },
             IMAGE_TOO_LARGE: {
-                nb: 'Fil er for stor.',
+                nb: 'Filen er for stor.',
             },
         },
     },
