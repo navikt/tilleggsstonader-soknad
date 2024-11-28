66d4e4a3e35b80e7d07bd49be1543b4a8e5fa898
Sigmund Berbom <sberbom@gmail.com>
2024-11-28 09:08:05 +0100

    Rett opp feilmelding tekster

--- src/frontend/tekster/filopplasting.ts
+++ src/frontend/tekster/filopplasting.ts
@@ -3,7 +3,6 @@ import { MAKS_FILSTØRRELSE_FORMATTERT } from '../components/Filopplaster/utils'
 import { TekstElement } from '../typer/tekst';
 
 export const teksterFeilmeldinger: {
-    enFil: TekstElement<string>;
     maksstørrelse: TekstElement<string>;
     filtype: TekstElement<string>;
     feilmeldinger: {
@@ -11,14 +10,11 @@ export const teksterFeilmeldinger: {
         fra_dokument: Record<FeilmeldingerFraDokument, TekstElement<string>>;
     };
 } = {
-    enFil: {
-        nb: `Må laste opp en og en fil`,
-    },
     maksstørrelse: {
-        nb: `For stor (maksimal filstørrelse er ${MAKS_FILSTØRRELSE_FORMATTERT}).`,
+        nb: `Filen er for stor (maksimal filstørrelse er ${MAKS_FILSTØRRELSE_FORMATTERT}).`,
     },
     filtype: {
-        nb: 'Ugyldig filtype.',
+        nb: 'Ugyldig filtype. Tillatte filtyper er .pdf, .png, .jpg og .jpeg.',
     },
     feilmeldinger: {
         generisk: {
@@ -26,7 +22,7 @@ export const teksterFeilmeldinger: {
         },
         fra_dokument: {
             IMAGE_DIMENSIONS_TOO_SMALL: {
-                nb: 'Bilde du har forsøkt å laste opp er for lite. Bilde må være større enn 400x400 piksler.',
+                nb: 'Bildet du har forsøkt å laste opp er for lite. Bildet må være større enn 400x400 piksler.',
             },
             IMAGE_TOO_LARGE: {
                 nb: 'Filen er for stor.',
