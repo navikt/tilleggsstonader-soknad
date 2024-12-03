5562c84127b30d8c8e2d457055df953cb9e37902
Sigmund Berbom <44610513+sberbom@users.noreply.github.com>
2024-12-03 11:01:20 +0100

    Legg til tidligere fullført vgs info boks (#424)

--- src/frontend/læremidler/steg/2-utdanning/HarTidligereFullførtVgs.tsx
+++ src/frontend/læremidler/steg/2-utdanning/HarTidligereFullførtVgs.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
 
+import { Alert, Heading, VStack } from '@navikt/ds-react';
+
 import { BlåVenstreRammeContainer } from '../../../components/BlåVenstreRammeContainer';
 import LocaleRadioGroup from '../../../components/Teksthåndtering/LocaleRadioGroup';
+import LocaleTekst from '../../../components/Teksthåndtering/LocaleTekst';
 import { EnumFelt } from '../../../typer/skjema';
 import { JaNei } from '../../../typer/søknad';
 import { Feilmelding } from '../../../typer/validering';
@@ -19,14 +22,25 @@ export const HarTidligereFullførtVgs: React.FC<Props> = ({
     feilmelding,
 }) => {
     return (
-        <BlåVenstreRammeContainer>
-            <LocaleRadioGroup
-                id={feilmelding?.id}
-                tekst={utdanningTekster.radio_har_fullført_vgs}
-                onChange={oppdaterHarTidligereFullførtVgs}
-                value={harTidligereFullførtVgs?.verdi || []}
-                error={feilmelding?.melding}
-            />
-        </BlåVenstreRammeContainer>
+        <VStack gap={'4'}>
+            <BlåVenstreRammeContainer>
+                <LocaleRadioGroup
+                    id={feilmelding?.id}
+                    tekst={utdanningTekster.radio_har_fullført_vgs}
+                    onChange={oppdaterHarTidligereFullførtVgs}
+                    value={harTidligereFullførtVgs?.verdi || []}
+                    error={feilmelding?.melding}
+                />
+            </BlåVenstreRammeContainer>
+
+            {harTidligereFullførtVgs?.verdi === 'NEI' && (
+                <Alert variant={'info'}>
+                    <Heading spacing size="small" level="3">
+                        <LocaleTekst tekst={utdanningTekster.har_fullført_vgs_info_boks_header} />
+                    </Heading>
+                    <LocaleTekst tekst={utdanningTekster.har_fullført_vgs_info_boks} />
+                </Alert>
+            )}
+        </VStack>
     );
 };
