01c0c9b671147cac65ed65b2a61f88457d7be32e
Nibedita.Pahari <nibedita.pahari@nav.no>
2024-12-04 13:44:46 +0100

    Settet opp feature toogle for harBehandling componenten.
    Co-authored-by: Runar Skagestad <runar.skagestad@nav.no>

--- src/frontend/components/RootRoute.tsx
+++ src/frontend/components/RootRoute.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 
+import Environment from '../api/Environment';
 import HarBehandlingSide from '../barnetilsyn/HarBehandlingSide';
 import { usePerson } from '../context/PersonContext';
 import { Stønadstype } from '../typer/stønadstyper';
@@ -11,8 +12,9 @@ interface RootRouteProps {
 
 export const RootRoute: React.FC<RootRouteProps> = ({ stønadstype, forside }) => {
     const { harBehandling } = usePerson();
+    const env = Environment();
     const [visHarBehandlingSide, settVisHarBehandlingSide] = useState<boolean>(harBehandling);
-    if (visHarBehandlingSide) {
+    if (visHarBehandlingSide && (env.miljø === 'preprod' || env.miljø === 'local')) {
         return (
             <HarBehandlingSide
                 stonadstype={stønadstype}
