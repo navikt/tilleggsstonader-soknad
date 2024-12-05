77933e00747bc8884f1f8521e380681180483dc0
Sigmund Berbom <sberbom@gmail.com>
2024-12-05 12:25:29 +0100

    Ta høyde for inegen registeraktiviteter fra backend

--- src/frontend/læremidler/steg/2-utdanning/UtdanningUtils.ts
+++ src/frontend/læremidler/steg/2-utdanning/UtdanningUtils.ts
@@ -3,17 +3,14 @@ import { EnumFelt, EnumFlereValgFelt } from '../../../typer/skjema';
 import { AnnenUtdanningType } from '../../typer/søknad';
 
 export const harValgtAktivitetPåVgsNivå = (
-    nyeValgteAktiviteter: EnumFlereValgFelt<string> | undefined,
+    valgteAktiviteter: EnumFlereValgFelt<string> | undefined,
     registerAktiviteter: Record<string, RegisterAktivitetMedLabel> | undefined,
     annenUtdanning: EnumFelt<AnnenUtdanningType> | undefined
 ) => {
-    if (!registerAktiviteter) {
-        return false;
-    }
-    return nyeValgteAktiviteter?.verdier.some((aktivitet) => {
-        return (
-            annenUtdanning?.verdi === AnnenUtdanningType.VIDEREGÅENDE ||
-            registerAktiviteter[aktivitet.verdi]?.erUtdanningPåVgsNivå
-        );
-    });
+    return (
+        annenUtdanning?.verdi === AnnenUtdanningType.VIDEREGÅENDE ||
+        valgteAktiviteter?.verdier.some(
+            (aktivitet) => registerAktiviteter?.[aktivitet.verdi]?.erUtdanningPåVgsNivå ?? false
+        )
+    );
 };
