496901f850e10e8a0fefd847ccbceaf32682b125
Nibedita.Pahari <nibedita.pahari@nav.no>
2024-12-05 14:55:59 +0100

    Har lagt til en status for har-behandling api kall da neste komponenten skal vise når status er true

--- src/frontend/components/Søknadside/SjekkBehandlingStatus.tsx
+++ src/frontend/components/Søknadside/SjekkBehandlingStatus.tsx
@@ -5,15 +5,19 @@ import { Stønadstype } from '../../typer/stønadstyper';
 
 const useSjekkBehandlingStatus = (stonadstype: Stønadstype) => {
     const [harBehandling, setHarBehandling] = useState<boolean | false>(false);
+    const [status, setStatus] = useState<boolean | false>(false);
 
     useEffect(() => {
         hentBehandlingStatus(stonadstype)
             .then(setHarBehandling)
             .catch(() => {
                 setHarBehandling(false);
+            })
+            .finally(() => {
+                setStatus(true);
             });
     }, [stonadstype]);
-    return { harBehandling };
+    return { harBehandling, status };
 };
 
 export default useSjekkBehandlingStatus;
