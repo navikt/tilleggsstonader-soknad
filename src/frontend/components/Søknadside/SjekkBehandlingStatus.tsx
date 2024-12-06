4fea484979e1284c4a5670453e0ed815243c28f8
Nibeditanav <nibedita.pahari@nav.no>
2024-12-06 09:06:59 +0100

    Viser ny komponenter etter alle api kall (#430)
    * Har lagt til en status for har-behandling api kall da neste komponenten skal vise når status er true

--- src/frontend/components/Søknadside/SjekkBehandlingStatus.tsx
+++ src/frontend/components/Søknadside/SjekkBehandlingStatus.tsx
@@ -4,16 +4,20 @@ import { hentBehandlingStatus } from '../../api/api';
 import { Stønadstype } from '../../typer/stønadstyper';
 
 const useSjekkBehandlingStatus = (stonadstype: Stønadstype) => {
-    const [harBehandling, setHarBehandling] = useState<boolean | false>(false);
+    const [harBehandling, setHarBehandling] = useState<boolean>(false);
+    const [harLastetBehandlingsstatus, setHarLastetBehandlingsstatus] = useState<boolean>(false);
 
     useEffect(() => {
         hentBehandlingStatus(stonadstype)
             .then(setHarBehandling)
             .catch(() => {
                 setHarBehandling(false);
+            })
+            .finally(() => {
+                setHarLastetBehandlingsstatus(true);
             });
     }, [stonadstype]);
-    return { harBehandling };
+    return { harBehandling, harLastetBehandlingsstatus };
 };
 
 export default useSjekkBehandlingStatus;
