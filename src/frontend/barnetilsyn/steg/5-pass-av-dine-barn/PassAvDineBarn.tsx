68781deca62ac8f9201c80403c4f338cb839de80
Nibedita.Pahari <nibedita.pahari@nav.no>
2025-03-04 12:22:08 +0100

    implementert "Når har du utgifter til pass? " spørsmål når "Har du utgifter til pass " er nei. endreing i PassAvDineBarn.tsx

--- src/frontend/barnetilsyn/steg/5-pass-av-dine-barn/PassAvDineBarn.tsx
+++ src/frontend/barnetilsyn/steg/5-pass-av-dine-barn/PassAvDineBarn.tsx
@@ -81,6 +81,7 @@ const PassAvDineBarn = () => {
                         oppdaterBarnMedBarnepass={oppdaterBarnMedBarnepass}
                         valideringsfeil={valideringsfeil}
                         nullstillValideringsfeil={nullstillValideringsfeil}
+                        locale={locale}
                     />
                 ))}
             </VStack>
