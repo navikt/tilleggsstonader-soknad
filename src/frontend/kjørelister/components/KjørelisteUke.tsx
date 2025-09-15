504c604818ec9f38c20c16de51150014500f0f1f
Sigmund Berbom <sberbom@gmail.com>
2025-09-15 17:13:40 +0200

    💄 Oppdater utseende på Accordion.Header

--- src/frontend/kjørelister/components/KjørelisteUke.tsx
+++ src/frontend/kjørelister/components/KjørelisteUke.tsx
@@ -1,17 +1,36 @@
 import React from 'react';
 
-import { Accordion, BodyShort, VStack } from '@navikt/ds-react';
+import styled from 'styled-components';
+
+import { Accordion, BodyShort, HStack, Tag, VStack } from '@navikt/ds-react';
 
 import KjørelisteDag from './KjørelisteDag';
 import { finnDagerMellomFomOgTomInklusiv, tilDagMåned } from '../../utils/datoUtils';
 import { RammevedtakUke } from '../types/Rammevedtak';
 
+const StyledHeader = styled(Accordion.Header)`
+    .navds-accordion__header-content {
+        width: 100%;
+    }
+`;
 const KjørelisteUke: React.FC<{ uke: RammevedtakUke }> = ({ uke }) => {
     const dager = finnDagerMellomFomOgTomInklusiv(uke.fom, uke.tom);
 
     return (
         <Accordion.Item>
-            <Accordion.Header>{`Uke ${uke.ukeNummer} (${tilDagMåned(uke.fom)} - ${tilDagMåned(uke.tom)}) [Påbegynt]`}</Accordion.Header>
+            <StyledHeader>
+                <HStack justify={'space-between'}>
+                    <HStack gap={'2'}>
+                        <BodyShort size={'large'} weight={'semibold'}>
+                            {`Uke ${uke.ukeNummer}`}
+                        </BodyShort>
+                        <BodyShort>{`(${tilDagMåned(uke.fom)} - ${tilDagMåned(uke.tom)})`}</BodyShort>
+                    </HStack>
+                    <Tag variant={'neutral'} size={'small'}>
+                        Ikke utfylt
+                    </Tag>
+                </HStack>
+            </StyledHeader>
             <Accordion.Content>
                 <VStack gap={'2'}>
                     <BodyShort weight={'semibold'}>Hvilke dager kørte du?</BodyShort>
