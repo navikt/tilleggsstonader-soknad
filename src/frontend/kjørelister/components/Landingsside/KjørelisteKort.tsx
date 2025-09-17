8e20af036bace9841694620f9af2610aa19e22c1
Sigmund Berbom <sberbom@gmail.com>
2025-09-17 13:52:37 +0200

    💬 Bruk tekstlig format på dato perioder

--- src/frontend/kjørelister/components/Landingsside/KjørelisteKort.tsx
+++ src/frontend/kjørelister/components/Landingsside/KjørelisteKort.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import { BodyShort, LinkCard, Tag } from '@navikt/ds-react';
 
-import { formaterPeriode } from '../../../utils/formateringUtils';
+import { formaterPeriodeTekstlig } from '../../../utils/formateringUtils';
 import { Rammevedtak } from '../../types/Rammevedtak';
 
 export const KjørelisteKort: React.FC<{ rammevedtak: Rammevedtak }> = ({ rammevedtak }) => {
@@ -15,7 +15,7 @@ export const KjørelisteKort: React.FC<{ rammevedtak: Rammevedtak }> = ({ rammev
             </LinkCard.Title>
             <LinkCard.Description>
                 <BodyShort>
-                    Periode med kjøring: {formaterPeriode(rammevedtak.fom, rammevedtak.tom)}
+                    Periode med kjøring: {formaterPeriodeTekstlig(rammevedtak.fom, rammevedtak.tom)}
                 </BodyShort>
                 <BodyShort>Adresse: {rammevedtak.aktivitetsadresse}</BodyShort>
                 <BodyShort>{rammevedtak.reisedagerPerUke} dager per uke</BodyShort>
