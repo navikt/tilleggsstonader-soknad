06d95224dc6fb1a48ffe85d2b10a349a8d27c641
Sigmund Berbom <sberbom@gmail.com>
2026-03-18 12:28:15 +0100

    💄 Fiks stylingen WideAccordionHeader som mistet sin custom stil etter oppdatering av aksel

--- src/frontend/kjørelister/components/WideAccordionHeader.tsx
+++ src/frontend/kjørelister/components/WideAccordionHeader.tsx
@@ -3,8 +3,7 @@ import styled from 'styled-components';
 import { Accordion } from '@navikt/ds-react';
 
 export const WideAccordionHeader = styled(Accordion.Header)`
-    /*noinspection CssUnusedSymbol*/
-    .aksel-accordion__header-content {
-        width: 100%;
+    > :last-child {
+        flex-grow: 1;
     }
 `;
