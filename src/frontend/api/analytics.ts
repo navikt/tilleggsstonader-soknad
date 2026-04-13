181254e7854d6ff9b22b0a10da2f7317d9705f80
Runar Skagestad <runar.skagestad@nav.no>
2026-04-13 13:05:08 +0200

    🔥 Fjern amplitude-loggeren

--- src/frontend/api/analytics.ts
+++ src/frontend/api/analytics.ts
@@ -1,4 +1,4 @@
-import { getAmplitudeInstance, getAnalyticsInstance } from '@navikt/nav-dekoratoren-moduler';
+import { getAnalyticsInstance } from '@navikt/nav-dekoratoren-moduler';
 
 import Environment from './Environment';
 import { stønadstypeTilSkjemaId, stønadstypeTilSkjemanavn } from '../typer/skjemanavn';
@@ -26,63 +26,41 @@ type eventType =
     | 'accordion åpnet'
     | 'accordion lukket';
 
-const getAmplitudeLogger = () => {
-    if (Environment().miljø !== 'local') {
-        return getAmplitudeInstance('dekoratoren');
-    } else {
-        return null;
-    }
-};
-
-const sendUmamiEvent = (event: eventType, eventData: Record<string, unknown>) => {
-    getAnalyticsInstance(APP_NAVN)(event, eventData).catch(() => {
-        // ignorer; enten er det en feil med lastingen av Umami, eller så har brukeren ikke samtykket
-    });
-};
-
-const amplitudeLogger = getAmplitudeLogger();
-
-export const loggEventMedSkjema = (
+export const sendUmamiEvent = (
     event: eventType,
     stønadstype: Stønadstype,
     eventProperties?: Record<string, unknown>
 ) => {
-    if (amplitudeLogger) {
-        amplitudeLogger(event, {
-            app: APP_NAVN,
-            skjemanavn: stønadstypeTilSkjemanavn[stønadstype],
-            skjemaId: stønadstypeTilSkjemaId[stønadstype],
-            ...eventProperties,
-        });
-    }
     if (Environment().miljø === 'local') {
         // eslint-disable-next-line no-console
         console.log(
             `[BARE LOKALT] Sender umami-event med eventType=${event} og eventProperties=${JSON.stringify(eventProperties)}`
         );
     }
-    sendUmamiEvent(event, {
+    getAnalyticsInstance('tilleggsstonader-soknad')(event, {
         app: APP_NAVN,
         skjemanavn: stønadstypeTilSkjemanavn[stønadstype],
         skjemaId: stønadstypeTilSkjemaId[stønadstype],
         ...eventProperties,
+    }).catch(() => {
+        // enten feil med lastingen av Umami, eller så har brukeren ikke samtykket.
     });
 };
 
 export const loggSkjemaStartet = (stønadstype: Stønadstype) => {
-    loggEventMedSkjema('skjema startet', stønadstype);
+    sendUmamiEvent('skjema startet', stønadstype);
 };
 
 export const loggSkjemaStegFullført = (stønadstype: Stønadstype, steg: string) => {
-    loggEventMedSkjema('skjema steg fullført', stønadstype, { steg: steg });
+    sendUmamiEvent('skjema steg fullført', stønadstype, { steg: steg });
 };
 
 export const loggSkjemaInnsendtFeilet = (stønadstype: Stønadstype) => {
-    loggEventMedSkjema('skjema innsending feilet', stønadstype);
+    sendUmamiEvent('skjema innsending feilet', stønadstype);
 };
 
 export const loggSkjemaFullført = (stønadstype: Stønadstype) => {
-    loggEventMedSkjema('skjema fullført', stønadstype);
+    sendUmamiEvent('skjema fullført', stønadstype);
 };
 
 export const logNavigereEvent = (
@@ -90,21 +68,21 @@ export const logNavigereEvent = (
     destinasjon: string,
     lenketekst: string
 ) => {
-    loggEventMedSkjema('navigere', stønadstype, {
+    sendUmamiEvent('navigere', stønadstype, {
         destinasjon: destinasjon,
         lenketekst: lenketekst,
     });
 };
 
 export const loggAlertVist = (stønadstype: Stønadstype, variant: string, tekst: string) => {
-    loggEventMedSkjema('alert vist', stønadstype, {
+    sendUmamiEvent('alert vist', stønadstype, {
         variant: variant,
         tekst: tekst,
     });
 };
 
 export const loggBesøk = (stønadstype: Stønadstype, url: string, sidetittel: string) => {
-    loggEventMedSkjema('besøk', stønadstype, {
+    sendUmamiEvent('besøk', stønadstype, {
         url: url,
         sidetittel: sidetittel,
     });
@@ -118,7 +96,7 @@ export const loggAccordionEvent = (
 ) => {
     const event = skalÅpnes ? 'accordion åpnet' : 'accordion lukket';
 
-    loggEventMedSkjema(event, stønadstype, {
+    sendUmamiEvent(event, stønadstype, {
         tekst: tekst,
         side: side,
     });
@@ -129,7 +107,7 @@ export const loggSkjemaSpørsmålBesvart = (
     spørsmål: string,
     svar: string
 ) => {
-    loggEventMedSkjema('skjema spørsmål besvart', stønadstype, {
+    sendUmamiEvent('skjema spørsmål besvart', stønadstype, {
         spørsmål: spørsmål,
         svar: svar,
     });
