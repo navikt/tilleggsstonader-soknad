aa28a84330fea92405a4acde7f4ab217e4343e2d
anderssonw <wandersson.98@gmail.com>
2026-06-23 14:14:35 +0200

    Endre 404 side til å passe søknader bedre

--- src/frontend/components/NotFound.tsx
+++ src/frontend/components/NotFound.tsx
@@ -1,24 +1,25 @@
-import { Box, VStack, Heading, BodyShort, Button } from '@navikt/ds-react';
-
-import { Container } from './Side';
+import { Box, VStack, Heading, BodyShort, Page, Link } from '@navikt/ds-react';
 
 export const NotFound: React.FC = () => {
     return (
-        <Container>
+        <Page.Block as="main" width="xl" gutters>
             <Box paddingBlock="space-80 space-64" data-aksel-template="404-v3">
-                <VStack gap="space-48" align="start">
-                    <VStack gap="space-16">
-                        <Heading level="1" size="large">
-                            Beklager, vi fant ikke siden
-                        </Heading>
-                        <BodyShort>
-                            Denne siden kan være slettet eller flyttet, eller det er en feil i
-                            lenken.
-                        </BodyShort>
-                    </VStack>
-                    <Button onClick={() => window.history.back()}>Gå tilbake</Button>
+                <VStack gap="space-16">
+                    <Heading level="1" size="large">
+                        Beklager, vi fant ikke siden
+                    </Heading>
+                    <BodyShort>
+                        Denne siden kan være slettet eller flyttet, eller det er en feil i lenken.
+                    </BodyShort>
+                    <BodyShort>
+                        Søknadsskjemaer for tilleggsstønader kan du finne på {` `}
+                        <Link href="https://www.nav.no/tilleggsstonader" inlineText>
+                            nav.no/tilleggsstonader
+                        </Link>
+                        .
+                    </BodyShort>
                 </VStack>
             </Box>
-        </Container>
+        </Page.Block>
     );
 };
