06444b64ca598a11b437b8f83c50079ca16920d2
sarahjelle <sara.hjelle@bekk.no>
2026-09-17 14:57:52 +0200

    Undøvendig å legge teksten i fellestekster

--- src/frontend/components/AlertIkkeRett.tsx
+++ src/frontend/components/AlertIkkeRett.tsx
@@ -3,7 +3,6 @@ import React from 'react';
 import { InfoCard } from '@navikt/ds-react';
 
 import { useSpråk } from '../context/SpråkContext';
-import { fellesTekster } from '../tekster/felles';
 import { TekstElement } from '../typer/tekst';
 
 export const AlertIkkeRett: React.FC<{ beskrivelse: TekstElement<string> }> = ({ beskrivelse }) => {
@@ -11,9 +10,13 @@ export const AlertIkkeRett: React.FC<{ beskrivelse: TekstElement<string> }> = ({
     return (
         <InfoCard data-color="info">
             <InfoCard.Header>
-                <InfoCard.Title>{fellesTekster.alert_ikke_rett_tittel[locale]}</InfoCard.Title>
+                <InfoCard.Title>{headerTekst[locale]}</InfoCard.Title>
             </InfoCard.Header>
             <InfoCard.Content>{beskrivelse[locale]}</InfoCard.Content>
         </InfoCard>
     );
+};
+
+const headerTekst: TekstElement<string> = {
+    nb: 'Ut fra svarene dine ser det ut til at du ikke har rett til denne pengestøtten',
 };
--- src/frontend/tekster/felles.ts
+++ src/frontend/tekster/felles.ts
@@ -20,7 +20,6 @@ export interface FellesInnhold {
     vedlegg_med_feil: TekstElement<string>;
     viktig_med_rett_opplysninger: TekstElement<InlineLenke>;
     avsluttOgLoggUt: TekstElement<string>;
-    alert_ikke_rett_tittel: TekstElement<string>;
 }
 
 export const fellesTekster: FellesInnhold = {
@@ -81,9 +80,6 @@ export const fellesTekster: FellesInnhold = {
     },
     avsluttOgLoggUt: {
         nb: 'Avslutt og logg ut',
-    },
-    alert_ikke_rett_tittel: {
-        nb: 'Ut fra svarene dine ser det ut til at du ikke har rett til denne pengestøtten',
     },
 };
 
